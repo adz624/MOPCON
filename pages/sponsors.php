6aa0d282762c4d4d1800ee0f1f62314a8513607d
Hikaru <hikaru@2be.com.tw>
2012-09-06 15:47:45 +0800

    Fix 版面

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -1,31 +1,18 @@
 <p class="anchor"><a href="#mainContenftKey" id="mainContentKey" accesskey="c">主要內容區</a></p>
 <div class="article">
-	<h2>活動行程</h2>
-	<dl class="sessionList">
-		<dt>活動議程表</dt>
-		<dd>目前活動尚在接洽中，尚無確定議程，先列出活動時間表。</dd>
-	</dl>
-	<table class="sessionDate">
-		<thead>
-			<tr>
-				<th scope="col">活動時間</th>
-				<th scope="col" colspan="2">週六</th>
-				<th scope="col" colspan="2">週日</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>9:00 ~ 9:50</td>
-				<td colspan="2">&nbsp;</td>
-				<td colspan="2">&nbsp;</td>
-			</tr>
-			<tr>
-				<td>9:50 ~ 10:00</td>
-				<td>休息</td>
-				<td>休息</td>
-				<td>休息</td>
-				<td>休息</td>
-			</tr>
-		</tbody>
-	</table>
+	<h2>主辦單位／贊助商</h2>
+	<div id="sponsorList">
+		<div class="main">
+			<h3>主辦單位</h3>
+			<p>感謝高雄市政府對產業與社群發展的支持</p>
+		</div>
+		<div class="second">
+			<h3>贊助商</h3>
+			<p>感謝以下各贊助單位</p>
+		</div>
+		<div class="special">
+			<h3>特別感謝</h3>
+			<p>感謝以下各單位之協助</p>
+		</div>
+	</div>
 </div>
