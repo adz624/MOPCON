899287aecf3dec3d1d4806a940701872c5ff4096
ericpi <hsuani.bi@gmail.com>
2012-09-12 19:56:43 +0800

    調整 hacker & geek sponsor 位置

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -4,6 +4,9 @@
 	<div id="sponsorList">
 		<div class="main">
 			<h3>主辦單位：<br />&nbsp;&nbsp;&nbsp;經濟部加工出口區管理處<br />&nbsp;&nbsp;&nbsp;國立中山大學南部促進產業發展研究中心</h3>
+			<p>感謝經濟部加工出口區對產業與社群發展的支持</p>
+		</div>
+		<div class="second">
 			<h3>執行單位：</h3>
 			<ul class="thumbnails">
 				<li class="span2"><a href="http://www.facebook.com/groups/KSDGroup/" class="thumbnail"><span class="fontLogo">KSDG</span></a></li>
@@ -11,13 +14,18 @@
 				<li class="span2"><a href="http://www.facebook.com/groups/mosut" class="thumbnail"><img alt="MOSUT logo" src="img/mosut.png" /></a></li>
 				
 			</ul>
-			<p>感謝經濟部加工出口區對產業與社群發展的支持</p>
 		</div>
 		<div class="second">
-			<h3>贊助商</h3>
+			<h3>Hacker級贊助商</h3>
 			<p>感謝以下各贊助單位</p>
 			<ul class="thumbnails">
 				<li class="span2 box_space"><a href="http://www.acomotech.com/" class="thumbnail"><img src="img/logo/acomotech-logo.png" alt="KKBOX" /></a></li>
+                        </ul>
+                </div>
+		<div class="second">
+			<h3>Geek級贊助商</h3>
+			<p>感謝以下各贊助單位</p>
+			<ul class="thumbnails">
 				<li class="span2 box_space"><a href="http://www.kkbox.com" class="thumbnail"><img src="img/logo/kkbox-logo.png" alt="KKBOX" /></a></li>
 				<li class="span2 box_space"><a href="http://www.mobilogics.com.tw" class="thumbnail"><img src="img/logo/mobilogics-logo.png" alt="明錄國際" /></a></li>
 				<li class="span2 box_space"><a href="http://www.moko365.com" class="thumbnail"><img src="img/logo/moko-logo.png" alt="Moko365" /></a></li>
