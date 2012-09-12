59a7d2d23186e6cc04292df535e2936731c0565e
Mikimoto <mikimoto.c@gmail.com>
2012-09-12 20:07:49 +0800

    修改首頁版型
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

---
+++ header.php
@@ -0,0 +1,63 @@
+<html lang="zh-TW">
+  <head> 
+    <meta charset="utf-8"> 
+    <title>MOPCON 2012 | Mobile Open Platform Conference</title> 
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="MOPCON 2012 | Mobile Open Platform Conference" property="og:title">
+    <meta content="http://mopcon.org" property="og:url">
+    <meta content="Mobile Open Platform Conference (MOPCON) MOPCON是全台第一個針對移動式裝置的技術研討會，希望大家共同來參與。" property="og:description">
+    <meta content="http://mopcon.org/img/snapshot.png" property="og:image">
+    <meta content="website" property="og:type">
+    <meta content="MOPCON 2012 | Mobile Open Platform Conference" property="og:site_name">
+    <meta name="description" content="MOPCON 2012 | Mobile Open Platform Conference">
+<link type="text/css" href="css/bootstrap/css/bootstrap.min.css" media="screen" rel="stylesheet">
+<link type="text/css" href="css/all.css" media="screen" rel="stylesheet">
+</head>
+<body data-twttr-rendered="true" data-spy="scroll" data-target=".subnav" data-offset="50">
+  <div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container" style="width: auto;">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <a id="main_logo" class="brand" href="http://test.mopcon.org"><img src="img/logo.png"/></a>
+        <div class="nav-collapse">
+          <ul class="nav">
+            <li class="active"><a href="http://test.mopcon.org">首頁</a></li>
+            <li class="active"><a href="session.html">議程</a></li>
+            <li class="active"><a href="#">社群</a></li>
+            <li class="active"><a href="#">報名</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">其他 <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">主辦/贊助商</a></li>
+                <li><a href="#">贊助資訊</a></li>
+                <li class="divider"></li>
+                <li><a href="#">交通</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav pull-right">
+            <li class="divider-vertical"></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Language <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">繁體中文</a></li>
+                <li><a href="#">English</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div>
+    </div><!-- /navbar-inner -->
+    <div id="functionSeprate"></div>
+  </div><!-- /navbar -->
+
+
+<script src="scripts/jquery-1.8.1.min.js"></script>
+<script src="css/bootstrap/js/bootstrap.js"></script>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+  $page = "home";
+  
+  include_once("header.php");
+?>
