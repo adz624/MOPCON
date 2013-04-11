5d66a7f242485cbb586bfa4dbfa61d56bfb45c3f
Cd Chen <cdchen@nicestudio.com.tw>
2013-04-11 17:01:44 +0800

    Add google analytics scripts.

--- header.php
+++ header.php
@@ -34,3 +34,14 @@ $(function () {
   js.src = "//connect.facebook.net/zh_TW/all.js#xfbml=1&appId=191415824227657";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34651129-1']);
+  _gaq.push(['_setDomainName', 'mopcon.org']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
