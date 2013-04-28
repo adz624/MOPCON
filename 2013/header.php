230f6c3a655c80772c2110062519366d923cf2b5
Cd Chen <cdchen@nicestudio.com.tw>
2013-04-28 23:12:50 +0800

    修正年份。

--- 2013/header.php
+++ 2013/header.php
@@ -1,16 +1,16 @@
     <meta charset="utf-8"> 
-    <title>MOPCON 2012 | Mobile Open Platform Conference</title> 
+    <title>MOPCON 2013 | Mobile Open Platform Conference</title> 
     <!--[if lt IE 9]>
       <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="MOPCON 2012 | Mobile Open Platform Conference" property="og:title">
+    <meta content="MOPCON 2013 | Mobile Open Platform Conference" property="og:title">
     <meta content="http://mopcon.org" property="og:url">
-    <meta content="Mobile Open Platform Conference (MOPCON) MOPCON是全台第一個針對移動式裝置的技術研討會，希望大家共同來參與。" property="og:description">
+    <meta content="Mobile Open Platform Conference (MOPCON) MOPCON 是全台第一個針對移動式裝置的技術研討會，希望大家共同來參與。" property="og:description">
     <meta content="http://mopcon.org/img/snapshot.png" property="og:image">
     <meta content="website" property="og:type">
-    <meta content="MOPCON 2012 | Mobile Open Platform Conference" property="og:site_name">
-    <meta name="description" content="MOPCON 2012 | Mobile Open Platform Conference">
+    <meta content="MOPCON 2013 | Mobile Open Platform Conference" property="og:site_name">
+    <meta name="description" content="MOPCON 2013 | Mobile Open Platform Conference">
     <meta name="google-translate-customization" content="872662db03c1ade2-296aa6b139f3956d-gb831f9b020eaa381-a"></meta>
 <link type="text/css" href="css/bootstrap/css/bootstrap.min.css" media="screen" rel="stylesheet">
 <link type="text/css" href="css/all.css" media="screen" rel="stylesheet">
