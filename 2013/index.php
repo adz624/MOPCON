9a442807e2bc71670d80a8809ca612441f4dd96e
Cd Chen <cdchen@nicestudio.com.tw>
2013-04-28 21:12:12 +0800

    加入 2013 介紹文字。

--- 2013/index.php
+++ 2013/index.php
@@ -6,50 +6,66 @@
   <body data-twttr-rendered="true" data-spy="scroll" data-target=".subnav" data-offset="50">
     <?php include_once("navbar.php");?>
     <div class="container" id="all">
-    <div class="space"></div>
-    <div class="" id="bd">
-  <div class="span4 offset1">
-      <h3>MOPCON 2012</h3>
-      <p>MOPCON 全名為 Mobile Open Platform Conference，為非營利的技術研討會，成立宗旨為針對移動通訊領域結合實際的產業面與工程研發(平台工具、後台服務、移動應用)做比較深入的探討。</p>
-      <p>希望藉由年度固定聚會持續專注這個領域, 培養台灣移動式領域人才或促使更多人投入這個領域的發展，並增加此領域廠商的能見度與第三方各種合作機會。我們將會邀請國外移動式領域廠商、專家增加與國內廠商、開發者更多交流與互動的機會。</p>
-  </div>
-
-  <div class="span5 hidden-phone"><img src="img/main_m02.png"/></div>
-  <div class="span1"></div>
-</div>
-
-    <div class="span9 well offset1">
-    <div class="span7">
-    <h3>報名資訊</h3>
-    <ul class="sessionList">
-		  <li><i class="icon-time"></i>&nbsp;&nbsp;時間：2012/10/27-28（六日兩天）</li>
-			<li><i class="icon-road"></i>&nbsp;&nbsp;地點：高雄軟體園區A棟頂樓海景會議廳</li>
-			<li><i class="icon-user"></i>&nbsp;&nbsp;主題：MOPCON 2012議程主軸為「電子票劵」，此議題之應用因近年手機硬體相機、NFC，軟體如雲端服務、QR code等技術發展逐漸成形，預計明年將發酵引發產業中新應用，故MOPCON將針對此主題尋求相關實作經驗的工程人員與廠商來產業上、中、下游進行完整探討與經驗分享</li>
-		</ul>
-		<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="btn btn-success btn-large" href="http://registrano.com/events/mopcon2012" target="_blank">查看有誰來過</a></p>
-  </div>
-  </div>
-        <div class="hidden_box"></div>
-        <div class="span10 offset1 container-fluid">
-                <legend>贊助商</legend>
-                <ul class="thumbnails">
-                    <li class="thumbnail"><a rel="external" href="http://www.acomotech.com/"><img src="img/logo/acomotech_f.png" alt="杏昕科技" /></a>
-                    <li class="thumbnail"><a rel="external" href="http://www.EZoUI.com/"><img src="img/logo/Ezo_f.png" alt="中冠資訊" /></a>
-                    <li class="thumbnail"><a rel="external" href="http://www.kkbox.com"><img src="img/logo/kkbox_f.png" alt="KKBOX" /></a>
-                    <li class="thumbnail"><a rel="external" href="http://www.mobilogics.com.tw"><img src="img/logo/mobilogics_f.png" alt="明錄國際" /></a>
-                    <li class="thumbnail"><a rel="external" href="http://www.moko365.com"><img src="img/logo/moko_f.png" alt="Moko365" /></a>
-                    <li class="thumbnail"><a rel="external" href="http://www.moregeek.com"><img src="img/logo/moregeek_f.png" alt="摩鉅科技" /></a>
-                    <li class="thumbnail"><a rel="external" href="http://www.wiwistock.com.tw"><img src="img/logo/wiwistock_f.png" alt="鈞達資訊" /></a>
-                </ul>
+      <div class="space">
+      </div>
+      <div class="" id="bd">
+        <div class="span4 offset1">
+          <h3>MOPCON 2013</h3>
+          <p>行動應用近年成為媒體追逐之寵兒，從電子商務、數位內容、商業服務、消費應用無不積極行動相關領域發展。MOPCON 專注於行動軟體開發 / 應用技術之專業研討會，也是南部最大社群研討會，希望藉由軟體技術社群的力量，推動南北業界技術實務經驗交流，關注行動應用趨勢。</p>
+          <p>MOPCON 2012 首度於高雄軟體園區舉辦，超過 160 為移動式平台的軟、硬體開發人員與廠商與會，來自全台各地之業界菁英擔任講師，兩日 17 場議程，內容綜觀行動前端開發、雲端服務架構、底層系統與開發工具、服務維運經驗等。100% 受訪與會者對 MOPCON 2012 議程給予滿意以上的評價，更高達 95% 平等為十分滿意。</p>
+          <p>
+          </p>MOPCON 2013 將以 "I code, I spread, I connect" 作為主要精神意涵，議程將以行動開發技術議程為核心，引申到行動服務營運，更將涵蓋數位內容之行動應用發展。</p>
+          <p>本屆活動日期訂於 2013 年 10 月 26 ~ 27 日於高雄國際會議中心 (ICCK) 盛大舉行，請密切關注我們的發展。</p>
         </div>
-        <div class="span10 offset1 container-fluid">
-                <legend>特別感謝</legend>
-                <ul class="thumbnails">
-                    <li class="thumbnail"><a rel="external" href="http://www.ecc.stu.edu.tw/"><img src="img/logo/stu_logo_f.png" alt="樹德科技大學 電算中心" /></a>
-                </ul>
+      </div>
+
+      <div class="span5 hidden-phone">
+        <img src="img/main_m02.png"/>
+      </div>
+
+      <div class="span1">
+      </div>
+
+      <div class="span9 well offset1">
+        <div class="span7">
+          <h3>報名資訊</h3>
+          <ul class="sessionList">
+      		  <li><i class="icon-time"></i>&nbsp;&nbsp;時間：2012/10/27-28（六日兩天）</li>
+      			<li><i class="icon-road"></i>&nbsp;&nbsp;地點：高雄軟體園區A棟頂樓海景會議廳</li>
+      			<li><i class="icon-user"></i>&nbsp;&nbsp;主題：MOPCON 2012議程主軸為「電子票劵」，此議題之應用因近年手機硬體相機、NFC，軟體如雲端服務、QR code等技術發展逐漸成形，預計明年將發酵引發產業中新應用，故MOPCON將針對此主題尋求相關實作經驗的工程人員與廠商來產業上、中、下游進行完整探討與經驗分享</li>
+      		</ul>
+      		<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="btn btn-success btn-large" href="http://registrano.com/events/mopcon2012" target="_blank">查看有誰來過</a></p>
         </div>
-        <div class="hidden_box"></div>
-  </div>
-    <?php include_once("footer.html");?>
+      </div>
+
+      <div class="hidden_box">
+      </div>
+
+      <div class="span10 offset1 container-fluid">
+        <legend>贊助商</legend>
+        <!--
+        <ul class="thumbnails">
+            <li class="thumbnail"><a rel="external" href="http://www.acomotech.com/"><img src="img/logo/acomotech_f.png" alt="杏昕科技" /></a>
+            <li class="thumbnail"><a rel="external" href="http://www.EZoUI.com/"><img src="img/logo/Ezo_f.png" alt="中冠資訊" /></a>
+            <li class="thumbnail"><a rel="external" href="http://www.kkbox.com"><img src="img/logo/kkbox_f.png" alt="KKBOX" /></a>
+            <li class="thumbnail"><a rel="external" href="http://www.mobilogics.com.tw"><img src="img/logo/mobilogics_f.png" alt="明錄國際" /></a>
+            <li class="thumbnail"><a rel="external" href="http://www.moko365.com"><img src="img/logo/moko_f.png" alt="Moko365" /></a>
+            <li class="thumbnail"><a rel="external" href="http://www.moregeek.com"><img src="img/logo/moregeek_f.png" alt="摩鉅科技" /></a>
+            <li class="thumbnail"><a rel="external" href="http://www.wiwistock.com.tw"><img src="img/logo/wiwistock_f.png" alt="鈞達資訊" /></a>
+        </ul>
+        -->
+      </div>
+      <div class="span10 offset1 container-fluid">
+        <legend>特別感謝</legend>
+        <!--
+        <ul class="thumbnails">
+            <li class="thumbnail"><a rel="external" href="http://www.ecc.stu.edu.tw/"><img src="img/logo/stu_logo_f.png" alt="樹德科技大學 電算中心" /></a>
+        </ul>
+        -->
+      </div>
+      <div class="hidden_box">
+      </div>
+    </div>
+  <?php include_once("footer.html");?>
   </body>
 </html>
