9bed6ecf73b025318d7fb3f261e8c100e05db5fe
_will <begining1003@gmail.com>
2013-05-19 20:52:06 +0800

    新增社群 KaLUG 及 MIMU

--- 2013/social.php
+++ 2013/social.php
@@ -38,13 +38,32 @@
 				<h3 class="popover-title"><abbr title="台南開放原始碼系統使用者聚會">MOSUT</abbr></h3>
 				<div class="popover-content">
 					<p>台南開放原始碼系統使用者聚會</p>
-		    <p><a href="https://code.google.com/p/mosut/">官方網站</a></p>
-		</div>
+                    <p><a href="https://code.google.com/p/mosut/">官方網站</a></p>
+                </div>
 			</div>
-	</div>
+        </div>
+		<div class="section">
+			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/groups/kimugroup"><img src="img/kimu.png" alt="KIMU logo"></a></p>
+			<div class="popover bottom">
+				<div class="arrow">&nbsp;</div>
+				<h3 class="popover-title"><abbr title="高雄獨立遊戲開發者聚會">MIMU</abbr></h3>
+				<div class="popover-content">
+                    <p>高雄獨立遊戲開發者聚會(Kaohsiung Indies Meet Up)，簡稱KIMU。2012年由中山大學促進產業發展研究中心協助成立，是一個以高雄為活動據點，推動南臺灣獨立遊戲開發國際化與相關產業活動的非營利性社群組織。</p>
 				</div>
 			</div>
-		</div>
+        </div>
+		<div class="section">
+			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/kalug.tw"><img src="img/kalug.jpg" alt="KaLut logo"></a></p>
+			<div class="popover bottom">
+				<div class="arrow">&nbsp;</div>
+				<h3 class="popover-title"><abbr title="大高雄Linux使用者協會">KaLUG</abbr></h3>
+				<div class="popover-content">
+                    <p>Kaohsiung Linux User Group (KaLUG) [1] 是由一群熱愛 open source / free software 的高雄人所組成的團體，平日討論的主題有 Linux, Debian, Ubuntu, Vim, Emacs, Git, Python, shell script, Drupal, OpenStreetMap, Security ... 等。</p>
+                    <p><a href="http://kalug.linux.org.tw/">官方網站</a></p>
+                    <p><a href="https://plus.google.com/communities/113465528091946960161">G+</a></p>
+                </div>
+			</div>
+        </div>
 	</div>
 </div>
     </div>
