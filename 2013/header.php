5839d5784f5d82a0629d840436bd855cc4e1cebd
Mikimoto <mikimoto.c@gmail.com>
2013-06-06 06:45:30 +0800

    fixed 2013 navigator bar
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

--- 2013/header.php
+++ 2013/header.php
@@ -1,23 +1,22 @@
-    <meta charset="utf-8"> 
-    <title>MOPCON 2013 | Mobile Open Platform Conference</title> 
+    <meta charset="utf-8">
+    <title>行動科技應用開發者年會 | MOPCON 2013 | Mobile / Open / Platform Conference</title>
     <!--[if lt IE 9]>
       <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="MOPCON 2013 | Mobile Open Platform Conference" property="og:title">
+    <meta content="行動科技應用開發者年會 | MOPCON 2013 | Mobile / Open / Platform Conference" property="og:title">
     <meta content="http://mopcon.org" property="og:url">
-    <meta content="Mobile Open Platform Conference (MOPCON) MOPCON 是全台第一個針對移動式裝置的技術研討會，希望大家共同來參與。" property="og:description">
-    <meta content="http://mopcon.org/img/snapshot.png" property="og:image">
+    <meta content="Mobile Open Platform Conference (MOPCON) 行動科技應用開發者年會 是全台第一個針對移動式裝置的技術研討會，希望大家共同來參與。" property="og:description">
+    <meta content="http://mopcon.org/img/snapshot.jpg" property="og:image">
     <meta content="website" property="og:type">
-    <meta content="MOPCON 2013 | Mobile Open Platform Conference" property="og:site_name">
-    <meta name="description" content="MOPCON 2013 | Mobile Open Platform Conference">
+    <meta content="行動科技應用開發者年會 | MOPCON 2013 | Mobile / Open / Platform Conference" property="og:site_name">
+    <meta name="description" content="行動科技應用開發者年會 | MOPCON 2013 | Mobile / Open / Platform Conference">
     <meta name="google-translate-customization" content="872662db03c1ade2-296aa6b139f3956d-gb831f9b020eaa381-a"></meta>
-<link type="text/css" href="css/bootstrap/css/bootstrap.min.css" media="screen" rel="stylesheet">
-<link type="text/css" href="css/all.css" media="screen" rel="stylesheet">
-<!-- <style type="text/css"> @font-face { font-family: "ct10"; src: url(http://ds3.justfont.com/file/get/100001_0_30668557695/ct10_d3e8f81b2827.jpeg?type=eot); src: local(" "),      url(http://ds3.justfont.com/file/get/100001_0_30668557695/ct10_d3e8f81b2827.jpeg?type=ttf) format("truetype"),      url(http://ds3.justfont.com/file/get/100001_0_30668557695/ct10_d3e8f81b2827.jpeg?type=woff) format("woff");} #ct10 { font-family: "ct10"; }.ct10 { font-family: "ct10"; }
-</style> -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-<link type="text/css" rel="stylesheet" href="http://cdn.jsdelivr.net/bootstrap/2.3.1/css/bootstrap.min.css" />
+    <link type="text/css" href="css/bootstrap/css/bootstrap.min.css" media="screen" rel="stylesheet">
+    <link type="text/css" href="css/all.css" media="screen" rel="stylesheet">
+    <!-- <style type="text/css"> @font-face { font-family: "ct10"; src: url(http://ds3.justfont.com/file/get/100001_0_30668557695/ct10_d3e8f81b2827.jpeg?type=eot); src: local(" "),      url(http://ds3.justfont.com/file/get/100001_0_30668557695/ct10_d3e8f81b2827.jpeg?type=ttf) format("truetype"),      url(http://ds3.justfont.com/file/get/100001_0_30668557695/ct10_d3e8f81b2827.jpeg?type=woff) format("woff");} #ct10 { font-family: "ct10"; }.ct10 { font-family: "ct10"; }</style> -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+    <link type="text/css" rel="stylesheet" href="http://cdn.jsdelivr.net/bootstrap/2.3.1/css/bootstrap.min.css" />
 <script type="text/javascript">
 $(function () {
     $("a[rel='external']").click(function () {
