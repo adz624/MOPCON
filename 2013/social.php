0fe3759e85f206e5828f6c0ad095d441436e0c36
webbertsai <begining1003@gmail.com>
2013-06-15 19:37:07 +0800

    merge 2012 社群

--- 2013/social.php
+++ 2013/social.php
@@ -11,12 +11,12 @@
       <div class="article">
 	<div id="aboutUs">
 		<div class="section">
-			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/groups/KSDGroup/"><span class="fontLogo">KSDG</span></a></p>
+			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/groups/KSDGroup/"><img src="img/ksdg.png" alt="Kaohsiung Software Developer Group"</img></a></p>
 			<div class="popover bottom">
 				<div class="arrow">&nbsp;</div>
 				<h3 class="popover-title"><abbr title="Kaohsiung Software Developer Group">KSDG</abbr></h3>
 				<div class="popover-content">
-				 	<p>高雄從事軟體開發、網路技術開發者一起交流的園地,由中山大學促進產業發展研究中心主辦,多家業界軟體/網路公司支持,期成為高雄定期技術交流之常態性社群。</p>
+            <p>Kaohsiung Software Developer Group 是高雄從事軟體開發、網路技術開發者一起交流的園地，2012/05首度聚會活動，目前每月舉辦 Meet-up 及 Web Course 各一場，交流主題涵蓋各種軟體技術，歡迎對軟體開發技術有興趣的朋友可以踴躍參與。</p>
 				</div>
 			</div>
 		</div>
@@ -32,41 +32,42 @@
 			</div>
 		</div>
 		<div class="section">
-			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/groups/mosut"><img src="img/mosut-b.jpg" alt="MOSUT logo"></a></p>
+			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/groups/mosut"><img src="img/mosut-c.png" alt="MOSUT logo"></a></p>
 			<div class="popover bottom">
 				<div class="arrow">&nbsp;</div>
 				<h3 class="popover-title"><abbr title="台南開放原始碼系統使用者聚會">MOSUT</abbr></h3>
 				<div class="popover-content">
-					<p>台南開放原始碼系統使用者聚會</p>
-                    <p><a href="https://code.google.com/p/mosut/">官方網站</a></p>
-                </div>
-			</div>
-        </div>
-		<div class="section">
-			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/groups/kimugroup"><img src="img/kimu.png" alt="KIMU logo"></a></p>
-			<div class="popover bottom">
-				<div class="arrow">&nbsp;</div>
-				<h3 class="popover-title"><abbr title="高雄獨立遊戲開發者聚會">KIMU</abbr></h3>
-				<div class="popover-content">
-                    <p>高雄獨立遊戲開發者聚會(Kaohsiung Indies Meet Up)，簡稱KIMU。2012年由中山大學促進產業發展研究中心協助成立，是一個以高雄為活動據點，推動南臺灣獨立遊戲開發國際化與相關產業活動的非營利性社群組織。</p>
+                    <p>台南開放原始碼系統使用者聚會</p>
+                    <a href="https://code.google.com/p/mosut/">官方網站</a>
 				</div>
 			</div>
         </div>
-		<div class="section">
-			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/kalug.tw"><img src="img/kalug.jpg" alt="KaLut logo"></a></p>
+<!-- 		<div class="section">
+			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/groups/kimugroup"><img src="img/kimu.png" alt="KIMU logo"></a></p>
 			<div class="popover bottom">
 				<div class="arrow">&nbsp;</div>
-				<h3 class="popover-title"><abbr title="大高雄Linux使用者協會">KaLUG</abbr></h3>
+			<h3 class="popover-title"><abbr title="高雄獨立遊戲開發者聚會">KIMU</abbr></h3>
+				<div class="popover-content">
+					<p>高雄獨立遊戲開發者聚會(Kaohsiung Indies Meet Up)，簡稱KIMU。2012年由中山大學促進產業發展研究中心協助成立，是一個以高雄為活動據點，推動南臺灣獨立遊戲開發國際化與相關產業活動的非營利性社群組織。</p>
+                </div>
+			</div>
+        </div> -->
+		<div class="section">
+			<p class="myLogo"><a class="thumbnail" href="http://www.facebook.com/kalug.tw?fref=ts"><img src="img/kalug.jpg" alt="KaLut logo"></a></p>
+			<div class="popover bottom">
+				<div class="arrow">&nbsp;</div>
+			<h3 class="popover-title"><abbr title="大高雄Linux使用者協會">KaLUG</abbr></h3>
 				<div class="popover-content">
                     <p>Kaohsiung Linux User Group (KaLUG) [1] 是由一群熱愛 open source / free software 的高雄人所組成的團體，平日討論的主題有 Linux, Debian, Ubuntu, Vim, Emacs, Git, Python, shell script, Drupal, OpenStreetMap, Security ... 等。</p>
                     <p><a href="http://kalug.linux.org.tw/">官方網站</a></p>
                     <p><a href="https://plus.google.com/communities/113465528091946960161">G+</a></p>
+				</div>
                 </div>
 			</div>
         </div>
-	</div>
-</div>
     </div>
+</div>
+</div>
   </div>
     <?php include_once("footer.html");?>
   </body>
