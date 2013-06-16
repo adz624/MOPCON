bdf5a0de23c6866a81f9fd82f98c81dda148d95d
Mikimoto <mikimoto.c@gmail.com>
2013-06-16 20:31:01 +0800

    fixed worker's memo
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

--- 2012/activity.php
+++ 2012/activity.php
@@ -9,171 +9,310 @@
       <div class="space"></div>
       <div class="article">
 	      <div id="sponsorList">
-		<div class="offset1 span9 second well-large">
+
+					<div class="offset1 span9 second well-large">
 			<h3>照片集：</h3>
 			<ul class="thumbnails">
         <li class="thumbnail"><a href="gallery.php">MOPCON 2012照片集</a></li>
 			</ul>
-		</div>
-		<div class="offset1 span9 second well-large">
-			<h3>irc log：(感謝 chusaing 協助紀錄)</h3>
-			<ul class="thumbnails">
-        <li class="thumbnail"><a rel="external" href="irclog/day1.log">day 1 (10/27)</a></li>
-        <li class="thumbnail"><a rel="external" href="irclog/day2.log">day 2 (10/28)</a></li>
-			</ul>
-		</div>
-		<div class="offset1 span9 second well-large">
-<!--工作人員的話B -->
-			<h3>工作人員的話：</h3>
-			
-			<div id="about">							
+					</div>
+
+					<div class="offset1 span9 second well-large">
+			<h3>本屆金句：</h3>
+			<div id="about">
+
 				<div class="section">
 					<div class="popover bottom">
-						<p class="popover-title" >日落</p>
+						<p class="popover-title" >hlb (售票亭亭長)</p>
 						<div class="popover-content">
-						<p>感謝講師願意播時間分享，也感謝會眾熱情參與討論，有你們使 MOPCON 變得十分精彩！
-						<br></br>
-						</p>	
-					</div>
+							<p>這年頭報名都不是人在報名的，programmer 都是用 automatic tools<br></br></p>
+						</div>
 					</div>
 				</div>
-				
+
 				<div class="section">
 					<div class="popover bottom">
-						<p class="popover-title" >欣妍</p>
+						<p class="popover-title" >hlb (售票亭亭長)</p>
 						<div class="popover-content">
-						<p>很高興能擔任MOPCON的工作人員，見證了高雄宅氣濃度指數創歷史新高的兩天(誤)。
-						<br></br>
-						</p>	
-							<div class="worker">				
-								<div class="worker-group off">
-								  <a class="worker-title" href="#">ｍｏｒｅ</a>
-								  <div class="worker-content">
-									在這裡認識許多熱血的朋友，也深刻地感受到社群成員的投入與熱情，我在這當中玩得很愉快!
-									<br></br>
-								  </div>
-								</div>				
-							</div>
+							<p>個資法不是問題，良心才是問題<br></br></p>
+						</div>
 					</div>
-					</div>
-				</div>		
-				
-				<div class="section">
-					<div class="popover bottom">
-						<p class="popover-title" >丞浩</p>
-						<div class="popover-content">
-						<p>在經過這次大家事前事後的努力，加上很多前輩賞光，使得活動熱絡，大大的成功了研討會!
-						<br></br>
-						</p>	
-							<div class="worker">				
-								<div class="worker-group off">
-								  <a class="worker-title" href="#">ｍｏｒｅ</a>
-								  <div class="worker-content">
-									從中很多很多人努力的付出，使得很多不完美的我們，更加的完美，明年一定會更好，因為我們同在～～
-									<br></br>
-								  </div>
-								</div>				
-							</div>
-					</div>
-					</div>
-				</div>					
-								
+				</div>
+
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >Jserv</p>
 						<div class="popover-content">
-						<p>從 2004 年至今，我就一直活躍在台灣的科技研討會，且長期擔任議程講師，而籌辦 MOPCON還是這幾年來的首次較為深入參與。
-						<br></br>
-						</p>	
-							<div class="worker">				
-								<div class="worker-group off">
-								  <a class="worker-title" href="#">ｍｏｒｅ</a>
-								  <div class="worker-content">
-自從今年四月底，我搬到台灣南部生活後，就不時與 Mikimoto兄談到南部資訊業的狀況，而他當時提議籌辦研討會、嘗試讓相關從業人員有交流討論的事宜，說真的，我還只能想像是個茶會的形式。<br></br>
-近來在Mikimoto、Eric，還有中山大學南區促進產業發展研究中心的林小姐奔走下，MOPCON 終於如期舉辦。<br></br>
-在眾多鄉親的支持下，不僅我們有多元的議程分享，而且講者與聽眾的互動也不錯，無論在議程時段或者會後交流，這些都要感謝眾多活動人員的投入。<br></br>
-因為最近在成大資訊系開課，我這個新手教師幾乎多半的時間都花在與同學討論，又在近期參與一個startup 項目，
-為 MOPCON付出的時間不多，實在很遺憾，但過程中，的確學到不少籌備活動的想法，這與過往只是擔任講者，有非常大的差異。<br></br>
-來年，希望 MOPCON 可辦得更好，也期待鄉親們繼續支持。
-									<br></br>
-								  </div>
-								</div>				
-							</div>
-					</div>
+							<p>會寫 JavaScript 的開發者，就能用自己的技能，把手機搞爛。<br>
+								 以前這是 C/C++/Obj-C/Java 開發者的特權，現在 Firefox OS 把特權還給使用者。
+								 <br></br>
+							</p>
+						</div>
 					</div>
 				</div>
-	
+
 				<div class="section">
 					<div class="popover bottom">
-						<p class="popover-title" >ericpi</p>
+						<p class="popover-title" >Ericpi</p>
 						<div class="popover-content">
-						<p>從COSCUP、OSDC等活動見識到台灣社群的活躍,在台北生活期間感受到 GTUG、TOSSUG等聚會對軟體從業人員與產業的影響力。  
-						<br></br>
-						</p>	
-							<div class="worker">				
+							<p>這是個打群架的時代
+								 <br></br>
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="section">
+					<div class="popover bottom">
+						<p class="popover-title" >佚名</p>
+						<div class="popover-content">
+							<p>我當 App 開發者這麼久，我發現一件事，通常使用者不會 report crash，而是直接給一顆星評價。
+								 <br></br>
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="section">
+					<div class="popover bottom">
+						<p class="popover-title" >佚名</p>
+						<div class="popover-content">
+							<p>我擔任開發團隊 Coding Monkey，不過絕大部分是扮演 Monkey 這個角色
+								 <br></br>
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="section">
+					<div class="popover bottom">
+						<p class="popover-title" >佚名</p>
+						<div class="popover-content">
+							<p>當玩家線上玩遊戲時，線上沒玩家配對時，寫個 AI 讓他跟電腦玩，不過玩家可能不知道他正在跟電腦玩。
+								 <br></br>
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="section">
+					<div class="popover bottom">
+						<p class="popover-title" >佚名</p>
+						<div class="popover-content">
+							<p>找 Marketing 很難，找 Design 很難，找 Art 很難，要找到一個堪用的 RD…… 很簡單。
+								 <br></br>
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="section">
+					<div class="popover bottom">
+						<p class="popover-title" >Mikimoto</p>
+						<div class="popover-content">
+							<p>在 MOPCON 2012 聽了好多「這一輩子都不會再幹的事」。
+								 <br></br>
+							</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+					</div>
+
+					<div class="offset1 span9 second well-large">
+			<h3>irc log：(感謝 chusaing 協助紀錄)</h3>
+			<ul class="thumbnails">
+        <li class="thumbnail"><a rel="external" href="day1.php">day 1 (10/27) 共計 130 人與會</a></li>
+        <li class="thumbnail"><a rel="external" href="irclog/day2.log">day 2 (10/28) 共計 116 人與會</a></li>
+			</ul>
+					</div>
+
+					<div class="offset1 span9 second well-large">
+						<h3>工作人員的話：</h3>
+						<p>
+							<h4>召集人：</h4>
+							<div id="about">
+
+								<div class="section">
+									<div class="popover bottom">
+										<p class="popover-title" >Jserv</p>
+										<div class="popover-content">
+											<p>從 2004 年至今，我就一直活躍在台灣的科技研討會，且長期擔任議程講師，而籌辦 MOPCON 還是這幾年來的首次較為深入參與。
+								 				 <br></br>
+											</p>
+											<div class="worker">
+												<div class="worker-group off">
+								  				<a class="worker-title" href="#">ｍｏｒｅ</a>
+								  				<div class="worker-content">
+														自從今年四月底，我搬到台灣南部生活後，就不時與 Mikimoto兄談到南部資訊業的狀況，而他當時提議籌辦研討會、嘗試讓相關從業人員有交流討論的事宜，說真的，我還只能想像是個茶會的形式。<br></br>
+														近來在Mikimoto、Eric，還有中山大學南區促進產業發展研究中心的林小姐奔走下，MOPCON 終於如期舉辦。<br></br>
+														在眾多鄉親的支持下，不僅我們有多元的議程分享，而且講者與聽眾的互動也不錯，無論在議程時段或者會後交流，這些都要感謝眾多活動人員的投入。<br></br>
+														因為最近在成大資訊系開課，我這個新手教師幾乎多半的時間都花在與同學討論，又在近期參與一個startup 項目，
+														為 MOPCON付出的時間不多，實在很遺憾，但過程中，的確學到不少籌備活動的想法，這與過往只是擔任講者，有非常大的差異。<br></br>
+														來年，希望 MOPCON 可辦得更好，也期待鄉親們繼續支持。
+														<br></br>
+								  				</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+				<div class="section">
+					<div class="popover bottom">
+						<p class="popover-title" >Ericpi</p>
+						<div class="popover-content">
+							<p>從COSCUP、OSDC等活動見識到台灣社群的活躍,在台北生活期間感受到 GTUG、TOSSUG等聚會對軟體從業人員與產業的影響力。
+								 <br></br>
+							</p>
+							<div class="worker">
 								<div class="worker-group off">
 								  <a class="worker-title" href="#">ｍｏｒｅ</a>
 								  <div class="worker-content">
-									 很高興有機會能夠在南部能開始有這樣規模的研討會, 當然這一切都要感謝 Mikimoto熱血推坑、Jserv熱情聯繫跟Tricia細心執行, 希望2013能繼續 MOPCON 這個傳統, 讓南部社群也有發聲的舞台, 也感謝MOPCON 2012所有工作人員的辛勞!
-									<br></br>
+										很高興有機會能夠在南部能開始有這樣規模的研討會, 當然這一切都要感謝 Mikimoto熱血推坑、Jserv熱情聯繫跟Tricia細心執行, 希望2013能繼續 MOPCON 這個傳統, 讓南部社群也有發聲的舞台, 也感謝MOPCON 2012所有工作人員的辛勞!
+										<br></br>
 								  </div>
-								</div>				
+								</div>
 							</div>
-					</div>
+						</div>
 					</div>
 				</div>
-				
+
 				<div class="section">
 					<div class="popover bottom">
-						<p class="popover-title" >Tricia</p>
+						<p class="popover-title" >Mikimoto</p>
+						<div class="popover-content">
+						<p>MOPCON 2012 在匆促之下成軍，在我、Jserv與Ericpi彼此互相推坑的狀況下，很艱辛的狀況下總算是順利完成今年的活動，感謝各路好友、鄉親一路相挺，MOPCON 2012 也才有機會舉辦。
+						<br></br>
+						</p>
+							<div class="worker">
+								<div class="worker-group off">
+								  <a class="worker-title" href="#">ｍｏｒｅ</a>
+								  <div class="worker-content">
+										經過了 MOPCON 2012 的洗禮，我總算知道「向宇宙下訂單」的威力，
+										一切只因我向 Jserv、Ericpi 說了句「你看移動式平台前中後段，我們三個社團全包了，要不要來辦個聯合 Conference」
+										，後面的發展就在我跟 Ericpi 彼此用力推坑，Jserv 在旁搖旗吶喊下，MOPCON 2012 就此誕生。<br>
+										希望大家滿意今年的 MOPCON，請大家把缺點告訴我們，把優點告訴你的朋友們，讓更多人一起來參與 MOPCON。
+										今年
+									<br></br>
+								  </div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="section">
+					<div class="popover bottom">
+						<p class="popover-title" >Tricia (傳說中的神秘女總召)</p>
 						<div class="popover-content">
 						<p>承辦過很多活動，倒是第一次負責技術性的研討會！不懂技術，要辦此類研討會，真的得靠大家的協助與努力。
 						<br></br>
-						</p>	
-							<div class="worker">				
+						</p>
+							<div class="worker">
 								<div class="worker-group off">
 								  <a class="worker-title" href="#">ｍｏｒｅ</a>
 								  <div class="worker-content">
 									特別感謝所有的義工，不畏辛勞及路途的遙遠，從各地熱心前來幫忙。最後，也謝謝這次給與我們機會，實際出席參與的講者及與會者，這是個開始，也期盼能替南部的技術性人才與活動創造多一點能見度！加油！
 									<br></br>
 								  </div>
-								</div>				
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				</div>
+			</p>
+
+			<p>
+				<h4>籌備小組成員：</h4>
+				<div id="about">
+
+					<div class="section">
+						<div class="popover bottom">
+							<p class="popover-title" >日落</p>
+							<div class="popover-content">
+								<p>感謝講師願意播時間分享，也感謝會眾熱情參與討論，有你們使 MOPCON 變得十分精彩！
+									 <br></br>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="section">
+						<div class="popover bottom">
+							<p class="popover-title" >欣妍</p>
+							<div class="popover-content">
+								<p>很高興能擔任MOPCON的工作人員，見證了高雄宅氣濃度指數創歷史新高的兩天(誤)。
+									 <br></br>
+								</p>
+								<div class="worker">
+									<div class="worker-group off">
+								  	<a class="worker-title" href="#">ｍｏｒｅ</a>
+								  	<div class="worker-content">
+											在這裡認識許多熱血的朋友，也深刻地感受到社群成員的投入與熱情，我在這當中玩得很愉快!
+											<br></br>
+								  	</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				<div class="section">
+					<div class="popover bottom">
+						<p class="popover-title" >丞浩</p>
+						<div class="popover-content">
+						<p>在經過這次大家事前事後的努力，加上很多前輩賞光，使得活動熱絡，大大的成功了研討會!
+						<br></br>
+						</p>
+							<div class="worker">
+								<div class="worker-group off">
+								  <a class="worker-title" href="#">ｍｏｒｅ</a>
+								  <div class="worker-content">
+									從中很多很多人努力的付出，使得很多不完美的我們，更加的完美，明年一定會更好，因為我們同在～～
+									<br></br>
+								  </div>
+								</div>
 							</div>
 					</div>
 					</div>
-				</div>	
+				</div>
 
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >瑩瑋</p>
 						<div class="popover-content">
-						<p>很高興這次可以擔任MOPCON的活動人員，雖然自己到參加活動前還搞不清楚到底啥是MOPCON，
-不過得知有早餐、午餐、下午茶還有一件ｔ恤可打包回家，馬上義無反顧就報名參加了。
-						<br></br>
-						</p>	
-							<div class="worker">				
+							<p>很高興這次可以擔任MOPCON的活動人員，雖然自己到參加活動前還搞不清楚到底啥是MOPCON，
+								 不過得知有早餐、午餐、下午茶還有一件ｔ恤可打包回家，馬上義無反顧就報名參加了。
+								 <br></br>
+							</p>
+							<div class="worker">
 								<div class="worker-group off">
-								  <a class="worker-title" href="#">ｍｏｒｅ</a>
-								  <div class="worker-content">						
-二天活動下來，只能說阿宅們(誤)~不，是程式界中各路高手和自己想像中完全不一樣，沒有荷葉邊，沒有夾鞋拖，沒有鬍渣(小的自己掌嘴…)，熱血是基本條件；幽默風趣個個有；帥氣外表更是不在話下，提議下次要優先開放給年輕妹報名當工作人員。
-打完上面178個字，還是沒有講到本次活動的感言，想想應該是程式太深奧，所以小的也只能發表和研討會完全無關的個人心得呀(菸菸)，呵~
-歡迎大家參加2013年MOPCON！
-									<br></br>
+									<a class="worker-title" href="#">ｍｏｒｅ</a>
+								  <div class="worker-content">
+										二天活動下來，只能說阿宅們(誤)~不，是程式界中各路高手和自己想像中完全不一樣，沒有荷葉邊，沒有夾鞋拖，
+										沒有鬍渣(小的自己掌嘴…)，熱血是基本條件；幽默風趣個個有；帥氣外表更是不在話下，提議下次要優先開放給年輕妹報名當工作人員。
+										打完上面178個字，還是沒有講到本次活動的感言，想想應該是程式太深奧，
+										所以小的也只能發表和研討會完全無關的個人心得呀(菸菸)，呵~
+										歡迎大家參加2013年MOPCON！
+										<br></br>
 								  </div>
-								</div>				
+								</div>
 							</div>
-					</div>
+						</div>
 					</div>
 				</div>
-				
+
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >佳勳</p>
 						<div class="popover-content">
 						<p>第一次以工作人員的身分來參加技術研討會，感覺十分的新鮮!!在參與幫忙的過程中，認識許多推廣此活動的超強主辦人，也了解整個研討會的準備過程以及內容。
 						<br></br>
-						</p>	
-							<div class="worker">				
+						</p>
+							<div class="worker">
 								<div class="worker-group off">
 								  <a class="worker-title" href="#">ｍｏｒｅ</a>
 								  <div class="worker-content">
@@ -182,32 +321,32 @@
 									若未來還有機會，我下次一定還會來  MOPCON2013 !!
 									<br></br>
 								  </div>
-								</div>				
+								</div>
 							</div>
 					</div>
 					</div>
-				</div>	
-							
+				</div>
+
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >球球</p>
 						<div class="popover-content">
 						<p>首先感謝前輩們抬愛，讓我有機會擔任這次MOPCON的工作人員，參與這次的盛事，也很感謝其他組員幫忙我完成會前的佈署以及會場當天的支援。
 						<br></br>
-						</p>	
-							<div class="worker">				
+						</p>
+							<div class="worker">
 								<div class="worker-group off">
 								  <a class="worker-title" href="#">ｍｏｒｅ</a>
 								  <div class="worker-content">
 									以往自己參與像COSCUP、OSDC等相關活動時，總是看到工作人員辛苦的忙進忙出，今年總算有幸輪到自己上戰場體會一下了XD。未來如果有機會的話，希望可以繼續為自由軟體社群盡一點微薄之力。
 									<br></br>
 								  </div>
-								</div>				
+								</div>
 							</div>
 					</div>
 					</div>
-				</div>	
-			
+				</div>
+
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >智豪</p>
@@ -215,11 +354,11 @@
 						<p>這是我第一次參與大型的研討會，在研討會我學到很多很多東西，
 						也體會到團體精神的重要，希望以後可以再參與這類的活動。
 						<br></br>
-						</p>	
+						</p>
 					</div>
 					</div>
 				</div>
-			
+
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >仁傑</p>
@@ -227,51 +366,50 @@
 						<p>這是我第一次參與大型的研討會，在研討會中我學到很多東西，
 						也體會到團體精神的重要，希望以後可以再參與這類的活動。
 						<br></br>
-						</p>	
+						</p>
 					</div>
 					</div>
-				</div>			
-					
-					
+				</div>
+
+
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >John Doe</p>
 						<div class="popover-content">
 						<p>很感謝MOPCON 讓我有這次機會可以為大家服務，也讓我有機會可以接觸認識到很多資訊界的高手。謝謝各位！
 						<br></br>
-						</p>	
+						</p>
 					</div>
 					</div>
-				</div>		
-			
+				</div>
+
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >哲均</p>
 						<div class="popover-content">
 						<p>經由這次MOPCN的活動,聽完大家的經驗分享,實在受益良多,謝謝大家!!
 						<br></br>
-						</p>	
+						</p>
 					</div>
 					</div>
-				</div>			
-				
+				</div>
+
 				<div class="section">
 					<div class="popover bottom">
 						<p class="popover-title" >Toomore</p>
 						<div class="popover-content">
 						<p>終於可以不用一直上台北參加研討會…
 						<br></br>
-						</p>	
+						</p>
 					</div>
 					</div>
-				</div>		
-			</div>
+				</div>
+				</div>
+			</p>
+    			</div>
 
-<!--工作人員的話E -->					
-
-		</div>
-          </div>
-      </div>
+      	</div>
+    	</div>
     </div>
     <?php include_once("footer.html");?>
   </body>
