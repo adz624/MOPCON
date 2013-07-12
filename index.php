a40c2a0e386699fbb51efab4cc95f3e7ccafb3c0
ericpi <hsuani.bi@gmail.com>
2013-07-12 11:42:41 +0800

    統一用 mopcon.org domain

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 header("HTTP/1.1 302 Redirect");
-header("Location: /2013/");
-?>
+
+if('mopcon.org' == strtolower($_SERVER['HTTP_HOST'])) {
+    header("Location: /2013/");
+} else {
+    header("Location: http://mopcon.org/2013/");
+}
+
