6b8b7136efc14df40e23c64600951317a87c27fd
CQD <wesleyliao@kkbox.com>
2013-07-23 00:25:55 +0800

    讓 404 頁面不論有沒有被套用到 bootstrap 跟 all.css 看起來都會一致

--- err/404.php
+++ err/404.php
@@ -10,21 +10,26 @@
     // $fixer = "checksuexec ".escapeshellarg($_SERVER['DOCUMENT_ROOT'].$_SERVER['REQUEST_URI']);
     // echo `$fixer`;
   }
-  
+
   $full_url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 ?>
 <html lang="zh-TW">
   <head>
     <?php include_once("../2013/header.php");?>
     <style>
+    html{
+      background-color:white;
+    }
     body {
+      background-color:white;
       text-align:center;
       color:#444;
     }
-    .container {
+    .content {
       text-align:left;
       width:800px;
       margin:3em auto;
+             background-color:red;
     }
     h1 {
       font-size:180px;
@@ -52,12 +57,12 @@
     </style>
   </head>
   <body>
-    <div class="container">
+    <div class="content">
       <div class="text">
         <h1>404</h1>
         <div class="box">
         <p>找不到您想看的網頁<br><a rel="nofollow" href="<? echo $full_url ?>"><? echo $full_url ?></a>
-        <p>要不要到<a href="/">首頁</a>去看看呢？</p>
+        <p>要不要到<a href="/"> MOPCON 首頁</a>去看看呢？</p>
 
         </div>
       </div>
