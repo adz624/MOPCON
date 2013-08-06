56b62b57ab55e416f23723a60ec13e91efc8f578
Pei-yu <chaopeiyu@gmail.com>
2013-08-06 16:51:21 +0800

    modify the share button of google+, twitter and facebook

---
+++ 2013/share.php
@@ -0,0 +1,36 @@
+
+<!-- facebook -->    
+<!--<script>
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=226197467449987";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+</script>
+<div style="float:left;" class="fb-like" data-href="https://www.facebook.com/mopcon" data-width="650" data-layout="button_count" data-show-faces="false" data-send="false"></div>
+-->
+<div style="float:left; width:88px;">
+	<iframe src="//www.facebook.com/plugins/like.php?locale=en_US&href=http%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Freference%2Fplugins%2Flike&amp;width=450&amp;height=21&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;send=false&amp;appId=226197467449987" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true">
+    </iframe>
+</div>
+<!--  twitter  -->
+<div style="float:left; padding-left:5px; padding-right:5px;">|</div>
+<div style="float:left; width:140px;">
+    <span id="followers"></span>
+    <a  href="https://twitter.com/mopcon" class="twitter-follow-button" data-show-count="true" data-show-screen-name="false" lang="uk"></a>
+</div>
+<script>
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+</script>
+
+<!--   google+  -->
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
+	{parsetags: 'explicit'}
+</script>
+<div style="float:left; padding-left:5px; padding-right:5px;">|</div>
+<div style="float:left;width:80px;">
+	<div class="g-plusone" data-size="medium" data-href="http://mopcon.org/2013/"></div>
+</div>
+<script type="text/javascript">gapi.plusone.go();</script>
