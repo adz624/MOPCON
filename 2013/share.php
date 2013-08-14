ddf3fb03daf8be8bffd2271e5d1f1b5191350b40
CQD <thecqd@gmail.com>
2013-08-14 08:07:19 +0800

    在 Follow us 區塊增加連到 SNS 的連結 #44

--- 2013/share.php
+++ 2013/share.php
@@ -11,12 +11,13 @@
 </script>
 <div style="float:left;" class="fb-like" data-href="https://www.facebook.com/mopcon" data-width="650" data-layout="button_count" data-show-faces="false" data-send="false"></div>
 -->
-<div style="float:left; width:88px;">
+<div style="float:left; width:88px;padding-right:5px;margin-right:5px;border-right:1px solid #333">
+        <div><a href="https://www.facebook.com/mopcon">Facebook</a></div>
 	<iframe src="//www.facebook.com/plugins/like.php?locale=en_US&href=https%3A%2F%2Fwww.facebook.com%2Fmopcon&amp;width=450&amp;height=21&amp;colorscheme=light&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;send=false&amp;appId=226197467449987" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
 </div>
 <!--  twitter  -->
-<div style="float:left; padding-left:5px; padding-right:5px;">|</div>
-<div style="float:left; width:90px;">
+<div style="float:left; width:90px;padding-right:5px;margin-right:5px;border-right:1px solid #333">
+    <div><a href="https://twitter.com/mopcon">Twitter</a></div>
     <div id="titter-div">
     	<span id="followers"></span>
     	<a  href="https://twitter.com/mopcon" class="twitter-follow-button" data-show-count="true" data-show-screen-name="false" lang="uk"></a>
@@ -36,11 +37,10 @@
 </script>
 
 <!--   google+  -->
+<div><a href="https://plus.google.com/116768206884057057248">Google+</div>
 <script type="text/javascript" src="https://apis.google.com/js/plusone.js">
 	{parsetags: 'explicit'}
 </script>
-<div style="float:left; padding-left:5px; padding-right:5px;">|</div>
-<div style="float:left;width:80px;">
 	<div class="g-plusone" data-size="medium" data-href="http://mopcon.org/2013/"></div>
 </div>
 <script type="text/javascript">gapi.plusone.go();</script>
