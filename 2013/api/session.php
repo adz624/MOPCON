b133f6fbe8fcfa9f40c73725da19dc9ffca84de4
Evan Mac Mini <imac@mobilogics.com.tw>
2013-09-11 10:14:37 +0800

    增加測試 json 資料 #48

--- 2013/api/session.php
+++ 2013/api/session.php
@@ -34,18 +34,29 @@ $data['sessions'][] = array(
         );
 
 $data['sessions'][] = array(
-        "id"          => 221,
+        "id"          => 130,
+        "name"        => "Mikimoto假面的告白",
+        "speaker"     => "Mikimoto",
+        "speaker_bio" => "熱血宅宅",
+        "keyword"     => "開發工具",
+        "loc"         => "企業號儲藏室",
+        "start_time"  => strtotime("2013-10-26 15:10:00"),
+        "end_time"    => strtotime("2013-10-26 16:40:00")
+        );
+
+$data['sessions'][] = array(
+        "id"          => 210,
         "name"        => "進擊的Mikimoto",
         "speaker"     => "Mikimoto",
         "speaker_bio" => "熱血宅宅",
         "keyword"     => "iOS",
         "loc"         => "企業號收發室",
-        "start_time"  => strtotime("2013-10-27 13:30:00"),
-        "end_time"    => strtotime("2013-10-27 14:40:00")
+        "start_time"  => strtotime("2013-10-27 10:20:00"),
+        "end_time"    => strtotime("2013-10-27 11:20:00")
         );
 
 $data['sessions'][] = array(
-        "id"          => 222,
+        "id"          => 220,
         "name"        => "進擊的Mikimoto PartII",
         "speaker"     => "Mikimoto",
         "speaker_bio" => "熱血宅宅",
@@ -53,6 +64,39 @@ $data['sessions'][] = array(
         "loc"         => "企業號魚雷室",
         "start_time"  => strtotime("2013-10-27 13:30:00"),
         "end_time"    => strtotime("2013-10-27 14:40:00")
+        );
+
+$data['sessions'][] = array(
+        "id"          => 221,
+        "name"        => "Mikimotoo與娜美",
+        "speaker"     => "Mikimoto",
+        "speaker_bio" => "熱血宅宅",
+        "keyword"     => "數位內容",
+        "loc"         => "企業號艦橋",
+        "start_time"  => strtotime("2013-10-27 13:30:00"),
+        "end_time"    => strtotime("2013-10-27 14:40:00")
+        );
+
+$data['sessions'][] = array(
+        "id"          => 222,
+        "name"        => "Mikimotoo與涼宮春日的浪漫夏天",
+        "speaker"     => "Mikimoto",
+        "speaker_bio" => "熱血宅宅",
+        "keyword"     => "開發平台",
+        "loc"         => "企業號輪機室",
+        "start_time"  => strtotime("2013-10-27 13:30:00"),
+        "end_time"    => strtotime("2013-10-27 14:40:00")
+        );
+
+$data['sessions'][] = array(
+        "id"          => 230,
+        "name"        => "你所不知道的Mikimotoo",
+        "speaker"     => "Mikimoto",
+        "speaker_bio" => "熱血宅宅",
+        "keyword"     => "數位內容",
+        "loc"         => "企業號甲板",
+        "start_time"  => strtotime("2013-10-27 15:10:00"),
+        "end_time"    => strtotime("2013-10-27 16:40:00")
         );
 
 echo json_encode($data);
