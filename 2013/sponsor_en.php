abbebf3dca922fe33c92452dad01462b73e405b3
ericpi <ericpi@kkbox.com>
2013-09-24 17:31:24 +0800

    加入贊助 Mobilogics

--- 2013/sponsor_en.php
+++ 2013/sponsor_en.php
@@ -48,6 +48,7 @@
               <li class="thumbnail"><a id="wiwistock" rel="external" href="http://www.wiwistock.com.tw"><img src="img/logo/wiwistock-logo.png" alt="鈞達資訊" /></a>
               <li class="thumbnail"><a id="userxper" rel="external" href="http://www.userxper.com/"><img src="img/logo/userxper_logo.png" alt="悠識數位" /></a>
               <li class="thumbnail"><a id="moko365" rel="external" href="http://www.moko365.com/"><img src="img/logo/moko-logo.png" alt="Moko365" /></a>
+              <li class="thumbnail"><a id="mobilogics" rel="external" href="http://www.mobilogics.com.tw/"><img src="img/logo/mobilogics-r_logo.png" alt="Mobilogics" /></a>
             </ul>
 		      </div>
 		      <div class="offset1 span9 second well-large">
