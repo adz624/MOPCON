c8a5752f28153b98800bd6bff083451a7699d539
Mikimoto <mikimoto.c@gmail.com>
2013-10-20 07:29:46 +0800

    edit irc page
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

---
+++ 2013/irc.php
@@ -0,0 +1,25 @@
+<?php
+  $page = "irc";
+  $function['irc'] = "active";
+  $title = "IRC";
+  $og_descr = "MOPCON IRC Channel";
+  $page_file = $_SERVER['PHP_SELF'];
+?>
+<html lang="zh-TW" class="">
+  <head><?php include_once("header.php");?></head>
+  <body data-twttr-rendered="true" data-spy="scroll" data-target=".subnav" data-offset="50">
+    <?php include_once("navbar.php");?>
+    <div class="container" id="all">
+      <div class="space"></div>
+      <div class="article">
+        <div id="irc">
+          <div class="offset1 span9 second well-large">
+            <h3>Windows Phone 8</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include_once("footer.html");?>
+  </body>
+</html>
