306b693b0ceb2b43623ba8e5802d04932ec6aee9
Mikimoto <mikimoto.c@gmail.com>
2013-10-19 18:30:14 +0800

    fixed session data
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

---
+++ 2013/apps.php
@@ -0,0 +1,31 @@
+<?php
+  $page = "apps";
+  $function['apps'] = "active";
+  $title = "Apps";
+  $og_descr = "MOPCON Apps";
+  $page_file = $_SERVER['PHP_SELF'];
+?>
+<html lang="zh-TW" class="">
+  <head><?php include_once("header.php");?></head>
+  <body data-twttr-rendered="true" data-spy="scroll" data-target=".subnav" data-offset="50">
+    <?php include_once("navbar.php");?>
+    <div class="container" id="all">
+      <div class="space"></div>
+      <div class="article">
+        <div id="sponsorList">
+          <div class="offset1 span9 second well-large">
+            <h3>本屆金句：</h3>
+            <div id="about">
+              <div class="section">
+                <div class="popover bottom">
+                  <p class="popover-title" >hlb (售票亭亭長)</p>
+                  <div class="popover-content">
+                    <p>這年頭報名都不是人在報名的，programmer 都是用 automatic tools<br></br></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
