78657a61936898c20d2b6f67495895b860d95bd0
Mikimoto <mikimoto.c@gmail.com>
2013-10-19 19:09:23 +0800

    edit Apps pages
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

--- 2013/apps.php
+++ 2013/apps.php
@@ -12,19 +12,15 @@
     <div class="container" id="all">
       <div class="space"></div>
       <div class="article">
-        <div id="sponsorList">
+        <div id="appsList">
           <div class="offset1 span9 second well-large">
-            <h3>官方活動 Apps：</h3>
-            <div id="about" class="span8">
-              <div class="section">
-                <div class="popover bottom">
-                  <p class="popover-title" >hlb (售票亭亭長)</p>
-                  <div class="popover-content">
-                    <p>這年頭報名都不是人在報名的，programmer 都是用 automatic tools<br></br></p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <h3>iOS</h3>
+          </div>
+          <div class="offset1 span9 second well-large">
+            <h3>Android</h3>
+          </div>
+          <div class="offset1 span9 second well-large">
+            <h3>Windows Phone 8</h3>
           </div>
         </div>
       </div>
