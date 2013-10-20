ee8666e6302a79dffd342684707c2c20b40d4a03
Mikimoto <mikimoto.c@gmail.com>
2013-10-20 20:48:42 +0800

    edit irc page for iPad
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

--- 2013/irc.php
+++ 2013/irc.php
@@ -10,14 +10,16 @@
   <body data-twttr-rendered="true" data-spy="scroll" data-target=".subnav" data-offset="50">
     <?php include_once("navbar.php");?>
     <div class="container" id="all">
+      <div class="space"></div>
+      <div class="article">
         <div id="irc" class="span10 second well-large">
-          <div class="ircframe well well-sm">
+          <div class="ircframe">
             <h3>加入IRC聊天頻道</h3>
             <div style="text-align:center; vertical-align:middle;">
               <iframe src="http://webchat.freenode.net?channels=mopcon&uio=d4" width="570" height="600" style="border: 0;"></iframe>
             </div>
           </div>
-          <div class="irclogs well well-sm">
+          <div class="irclogs">
             <h3>活動實況文字轉播記錄</h3>
           </div>
         </div>
