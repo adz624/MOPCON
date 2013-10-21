b87603e2a07765a941191df433a828ecff3632eb
Mikimoto <mikimoto.c@gmail.com>
2013-10-21 20:03:40 +0800

    edit irc page for iPad
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

--- 2013/irc.php
+++ 2013/irc.php
@@ -26,10 +26,10 @@
               <h3>活動實況文字轉播記錄</h3>
               <div class="row">
                 <div class="span4 ircframe">
-                  <p>2013/10/26 (not yet)</p>
+                  <p align="center"><h4>2013/10/26 (not yet)<h4></p>
                 </div>
                 <div class="span4 ircframe">
-                  <p>2013/10/27 (not yet)</p>
+                  <p align="center">2013/10/27 (not yet)</p>
                 </div>
               </div>
             </div>
