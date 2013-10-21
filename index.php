f06c7557ed0987908015053d22276f22e97e97c3
Mikimoto <mikimoto.c@gmail.com>
2013-10-21 22:02:58 +0800

    merge to develop
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 header("HTTP/1.1 302 Redirect");
 
-if('test.mopcon.net' == strtolower($_SERVER['HTTP_HOST'])) {
+if('mopcon.org' == strtolower($_SERVER['HTTP_HOST'])) {
     header("Location: /2013/");
 } else {
-    header("Location: http://test.mopcon.net/2013/");
+    header("Location: http://mopcon.org/2013/");
 }
 
