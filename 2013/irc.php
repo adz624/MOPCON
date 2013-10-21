50228f8bb2e790e5343fa82f4fe892d7d76d595a
Mikimoto <mikimoto.c@gmail.com>
2013-10-21 17:09:58 +0800

    edit irc page for iPad
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

--- 2013/irc.php
+++ 2013/irc.php
@@ -13,11 +13,10 @@
       <div class="space"></div>
       <div class="article">
         <div id="irc" class="span10 second well-large">
-          <div class="ircframe">
+          <div class="span9">
             <h3>加入IRC聊天頻道</h3>
-            <div style="text-align:center; vertical-align:middle;">
-              <iframe src="http://webchat.freenode.net?channels=mopcon&uio=d4" width="570" height="600" style="border: 0;"></iframe>
-            </div>
+            <p>MOPCON 在 Freenote 註冊了一個 #mopcon 文字頻道，歡迎大家一起來哈拉與線上轉播。</p>
+            <p><a class="btn btn-success btn-large" target="_blank" rel="http://webchat.freenode.net?channels=mopcon&uio=d4">報名截止</a></p>
           </div>
           <div class="irclogs">
             <h3>活動實況文字轉播記錄</h3>
