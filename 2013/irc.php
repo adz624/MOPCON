567eab622bde30ad487f6605e46e9c839b87d870
Mikimoto <mikimoto.c@gmail.com>
2013-10-21 17:19:40 +0800

    edit irc page for iPad
    Signed-off-by: Mikimoto <mikimoto.c@gmail.com>

--- 2013/irc.php
+++ 2013/irc.php
@@ -13,13 +13,18 @@
       <div class="space"></div>
       <div class="article">
         <div id="irc" class="span10 second well-large">
-          <div class="span9">
-            <h3>加入IRC聊天頻道</h3>
-            <p>MOPCON 在 Freenote 註冊了一個 #mopcon 文字頻道，歡迎大家一起來哈拉與線上轉播。</p>
-            <p><a class="btn btn-success btn-large" target="_blank" rel="http://webchat.freenode.net?channels=mopcon&uio=d4">報名截止</a></p>
+          <div class="row">
+            <div class="span9">
+              <h3>加入IRC聊天頻道</h3>
+              <p>MOPCON 在 Freenote 註冊了一個 #mopcon 文字頻道，歡迎大家一起來哈拉與線上轉播。</p>
+              <p><a class="btn btn-success btn-large" target="_blank" rel="http://webchat.freenode.net?channels=mopcon&uio=d4">加入頻道討論</a></p>
+            </div>
           </div>
-          <div class="irclogs">
-            <h3>活動實況文字轉播記錄</h3>
+          <div class="row">
+            <div class="row span9">
+              <h3>活動實況文字轉播記錄</h3>
+              <p>建置中</p>
+            </div>
           </div>
         </div>
       </div>
