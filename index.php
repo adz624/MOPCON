d35a67d446bb04d792c8d580e457d180d67acfa7
ericpi <ericpi@kkbox.com>
2014-01-02 19:47:50 +0800

    把 2014 推向首頁!

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 header("HTTP/1.1 302 Redirect");
 
 if('mopcon.org' == strtolower($_SERVER['HTTP_HOST'])) {
-    header("Location: /2013/");
+    header("Location: /2014/");
 } else {
-    header("Location: http://mopcon.org/2013/");
+    header("Location: http://mopcon.org/2014/");
 }
 
