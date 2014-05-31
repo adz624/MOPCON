3c6f65d4077830247ad0b325f3d7093f37eb1173
CQD <cqd@hiigara.net>
2014-05-31 23:04:37 +0800

    首頁加上 Facebook 粉絲團連結

--- 2014/index.php
+++ 2014/index.php
@@ -10,6 +10,9 @@ $tpl->title = "MOPCON 2014";
 $tpl->mainClass = 'index';
 
 $tpl->content =<<<RAW_HTML
+<div style="margin-top:-2em; font-size:1.5em; line-height:2em;">
+<a href="https://www.facebook.com/mopcon/" alt="MOPCON Facebook 粉絲團" title="MOPCON Facebook 粉絲團" style="color:#56CAB6;"><i class="icon-facebook42"></i></a>
+</div>
 <img src="images/banner.png" style="margin:0 0 30px 0" alt="堅持在濁水溪以南舉辦的千人社群研討會，將於 2014/10/25 日到 2014/10/26 在高雄國際會議中心舉辦" >
 RAW_HTML;
 
