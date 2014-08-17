296d34f3ce847a8e32e3463b98e608e3dd3b6a55
CQD <cqd@hiigara.net>
2014-08-17 12:29:24 +0800

    把一些太大的圖片弄小一點; 加上 Ernest 的照片

--- 2014/src/speakers.php
+++ 2014/src/speakers.php
@@ -12,14 +12,14 @@ return [
         'company' => 'Golface',
         'title'   => '創辦人',
         'bio'     => '現為 Golface (綠夾克運動有限公司) 負責人，發現高爾夫球在台灣是很昂貴的運動，很多人希望透過這個運動建立人際關係，但國內高球場經營非常傳統，缺一套球場專屬物流系統，於是喜愛打高爾夫球的 Ian 看到這個需求，和團隊成員開發出高爾夫球場的數位化解決方案 Golface，從動態球道空拍攝影、即時資訊回報、便捷賽事紀錄管理，甚至包括球友間的社交平台都提供專項服務。目前 Golface已把整套解決方案，導入 Rolex 全球排名1000 大的高爾夫球場',
-        'pic'     => 'ian.jpg',
+        'pic'     => 'ian_500.jpg',
     ],
     'mhung' => [
         'name'    => 'Michael Hung (洪志鵬)',
         'company' => 'Mozilla Taiwan',
         'title'   => '行銷總監',
         'bio'     => '洪志鵬 (Michael Hung)，別名 Michaelsoft。現職為 Mozilla 台灣分公司資深行銷總監，曾任台灣微軟全球技術支援中心副總經理、昇陽電腦(Sun Microsystems) 行銷總監等職。亦曾為專職作家，著有《最後的江湖道義》等四本書',
-        'pic'     => 'Michael_Hung.jpg',
+        'pic'     => 'Michael_Hung_500.jpg',
     ],
     'keynes' => [
         'name'    => 'Keynes (鄭鎧尹)',
@@ -54,7 +54,6 @@ return [
         'company' => 'PAFERS',
         'title'   => '共同創辦人',
         'bio'     => '小時候玩 MS-DOS, OS/2, Windows 3.1,  Slackware，玩著玩著玩到大學申請少了零點多分沒法進資工系玩，改玩半導體製程和微小機電，幸運地進入台積研究所玩製程整合從研發、試產玩到量產。熱愛開放與不務正業的精神，曾經玩網路創業，現在上班在玩傳統產業 + 軟硬體整合，試著把無線通訊塞進健身器材中。下班參與 Mozilla 台灣社群 MozTW SUMO 專案，以及支援朋友們玩各種創業。夢想與大家玩出各種可能性。愛台灣，不解釋',
-//        'pic'     => 'ernest.jpg',
-        'pic'     => 'generic_speaker.png',
+        'pic'     => 'Ernest_profile_high_500.jpg',
     ],
 ];
