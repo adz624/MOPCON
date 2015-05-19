9c402ad130a490efdfdb1589119487fd59909d27
CQD <thecqd@gmail.com>
2015-05-19 23:48:11 +0800

    導向到 2015

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
-header("HTTP/1.1 302 Redirect");
-
-if('mopcon.org' == strtolower($_SERVER['HTTP_HOST'])) {
-    header("Location: /2014/");
-} else {
-    header("Location: http://mopcon.org/2014/");
-}
-
+header("Location: http://mopcon.org/2015/");
