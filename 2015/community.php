8ef3f91baa8b22f930038728b62fe93022a8e087
CQD <thecqd@gmail.com>
2015-07-27 02:36:47 +0800

    社群頁跟地圖頁加上不同的網頁標題

--- 2015/community.php
+++ 2015/community.php
@@ -1,5 +1,18 @@
 <?php
 include __DIR__ . '/src/init.php';
+
+$main = [
+    'tc' => [
+        'pagetitle' => '主辦社群',
+    ],
+    'en' => [
+        'pagetitle' => 'Organizing Communities',
+    ],
+];
+
+$main = getI18n($main);
+
 render('community.twig', [
     'pageid' => 'community',
+    'main' => $main,
 ]);
--- 2015/location.php
+++ 2015/location.php
@@ -1,5 +1,18 @@
 <?php
 include __DIR__ . '/src/init.php';
+
+$main = [
+    'tc' => [
+        'pagetitle' => '地圖',
+    ],
+    'en' => [
+        'pagetitle' => 'Location',
+    ],
+];
+
+$main = getI18n($main);
+
 render('location.twig', [
     'pageid' => 'location',
+    'main' => $main,
 ]);
