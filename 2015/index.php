978fd3f56c47a3543ff69ecf7c23778f2274d8f6
CQD <thecqd@gmail.com>
2015-07-27 01:10:45 +0800

    把 index 接上 PHP

---
+++ 2015/index.php
@@ -0,0 +1,5 @@
+<?php
+include __DIR__ . '/src/init.php';
+render('index.twig', [
+    'pageid' => 'index',
+]);
---
+++ 2015/src/init.php
@@ -0,0 +1,76 @@
+<?php
+include __DIR__ . "/../../vendor/autoload.php";
+
+//////////////////////////////////////////////////////////////////////////////
+function getLang()
+{
+    static $lang = null;
+    if ($lang) {
+        return $lang;
+    }
+
+    $supported_langs = [
+        'tc',
+        'en'
+    ];
+
+    if (isset($_GET['lang']) && in_array($_GET['lang'], $supported_langs)) {
+        $lang = $_GET['lang'];
+    } elseif(isset($_COOKIE['lang'])) {
+        $lang = $_COOKIE['lang'];
+    } else {
+        $lang = 'tc';
+    }
+    setcookie('lang', $lang);
+
+    return $lang;
+}
+
+function getI18n($msg_data)
+{
+    if (!is_array($msg_data) || 0 === count($msg_data)) {
+        return [];
+    }
+
+    $lang = getLang();
+
+    if (isset($msg_data[$lang])) {
+        return $msg_data[$lang];
+    }
+    if (isset($msg_data['tc'])) {
+        return $msg_data['tc'];
+    }
+    return reset($msg_data); // return first group
+}
+
+function render($template_name, $params)
+{
+    $main_msg = [
+        'tc' => [
+            'sitetitle' => '行動科技應用開發者年會 | MOPCON 2015 | Mobile / Open / Platform Conference',
+            'ogtitle' => '行動科技應用開發者年會 | MOPCON 2015 | Mobile / Open / Platform Conference',
+            'ogsitename' => '行動科技應用開發者年會 | MOPCON 2015 | Mobile / Open / Platform Conference',
+            'ogdesc' => '濁水溪以南最強大行動科技研討會，2015強勢回歸',
+            'navcommunity' => '社群',
+            'navlocation' => '地圖',
+            'navcfp' => '徵稿',
+        ],
+        'en' => [
+            'sitetitle' => 'MOPCON 2015 | Mobile / Open / Platform Conference',
+            'ogtitle' => 'MOPCON 2015 | Mobile / Open / Platform Conference',
+            'ogsitename' => 'MOPCON 2015 | Mobile / Open / Platform Conference',
+            'ogdesc' => 'Your favoeite conference for mobile technology in southern Taiwan is now back in 2015',
+            'navcommunity' => 'Community',
+            'navlocation' => 'Location',
+            'navcfp' => 'Call for paper',
+        ],
+    ];
+
+    $params = array_replace_recursive(
+        ['main' => getI18n($main_msg)],
+        $params
+    );
+    $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../twig');
+    $twig = new \Twig_Environment($loader);
+    echo $twig->render($template_name, $params);
+}
