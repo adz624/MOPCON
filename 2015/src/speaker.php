f8177c271bfd4cddd62ce610154135e9c12071f7
hungjie19 <hungjie19@gmail.com>
2015-10-07 03:01:13 +0800

    edit speaker info

--- 2015/src/speaker.php
+++ 2015/src/speaker.php
@@ -14,7 +14,7 @@ function getAllSpeakers()
         'sj' => [
             'name' => '周家弘 (SJ)',
             'pic' => 'speaker/sj.jpg',
-            'title' => 'Foxconn 工程師',
+            'title' => '鴻海 工程師',
             'bio' => "目前於鴻海擔任工程師，負責開發雲端服務，平常喜歡聽點音樂與寫點部落格 (Soul &amp; Shell Blog)。幾年前曾與日本富士康合作開發系統，從此奠定對軟體品質的堅持。熱愛開發新產品，近年來熱衷於 Web 技術，流連往返於程式設計與軟體架構。",
             'links' => [
                 'Facebook' => 'https://www.facebook.com/samejack',
@@ -24,7 +24,7 @@ function getAllSpeakers()
         'carl' => [
             'name' => 'Carl Su',
             'pic' => 'speaker/carl.jpg',
-            'title' => 'DevOps Engineer',
+            'title' => 'DevOps Software Engineer',
             'bio' => '具有軟體測試和 CI/CD 導入經驗，熟悉自動化工具和雲端平台。認同開源理念，平時亦參與開源專案 (Mozilla, Google, Docker)。樂於分享知識和經驗。除了公開演講，也會舉辦讀書會和工作坊。',
             'links' => [
                 'Facebook' => 'https://www.facebook.com/carl.su.9',
@@ -33,7 +33,7 @@ function getAllSpeakers()
         'benjai' => [
             'name' => '翟本喬 (Ben Jai)',
             'pic' => 'speaker/benjai.jpg',
-            'title' => '和沛科技創辦人暨總經理',
+            'title' => '和沛科技 創辦人暨總經理',
             'bio' => "翟本喬為台灣第一位跳級生，1999 年赴美取得紐約大學電腦科學博士，2003 年進入 Google，成功研發高效率單電壓電源供應器。2010 年回到台灣加入台達電子創立雲端技術中心。2013 年 6 月率原團隊成立和沛科技，積極開發符合雲端運算精神的新產品及服務，並對於台灣創新創業環境多有建言。",
             'links' => [
                 'Facebook' => 'https://www.facebook.com/ben.jai',
@@ -51,7 +51,7 @@ function getAllSpeakers()
         'muki' => [
             'name' => '吳姿穎 (Muki)',
             'pic' => 'speaker/muki.jpg',
-            'title' => '拾木空間總兼',
+            'title' => '拾木空間 總兼',
             'bio' => "拾木空間總兼，經營文具社群與文具銷售，同時也是網站設計師與講師。<br>著作包含 2 本 WordPress 書、1 本手帳整理術，以及譯作 2 本 WordPress 書。<br>喜愛推廣與學習 HTML, CSS, 與 jQuery，興趣是在跨界與自我挑戰中成長，從事過設計、前端、行銷與專案管理等工作。",
             'links' => [
                 'Facebook' => 'https://www.facebook.com/muki.wu',
@@ -60,7 +60,7 @@ function getAllSpeakers()
         'blue' => [
             'name' => '陳柏儒 (Blue)',
             'pic' => 'speaker/blue.jpg',
-            'title' => '聯發科技前端工程師',
+            'title' => '聯發科技 MTK 前端工程師',
             'bio' => "目前在聯發科擔任前端工程師，長期專注網頁前端標準技術。現行工作主要參與物聯網議題，致力於降低讓大眾進入硬體門檻的各種可能性。",
             'links' => [
                 'Facebook' => 'https://www.facebook.com/IamBlueChen',
@@ -69,7 +69,7 @@ function getAllSpeakers()
         'kny' => [
             'name' => '陳坤助 (KNY)',
             'pic' => 'speaker/kny.jpg',
-            'title' => '中華民國 App 跨界交流協會理事長',
+            'title' => 'KNY Co. 執行長',
             'bio' => "<ul><li>KNY Co. 執行長</li><li>中華民國 App 跨界交流協會 理事長</li><li>台灣開放資料聯盟App組 召集人</li><li>交通部資料開放諮詢小組 諮詢委員</li><li>曾獲 Google Play 頂尖開發人員徽章</li></ul>國小開始 Coding，全端獨立開發超過 10 年，長期關注與推動台灣 App 與 Open Data 的發展。希望能透過科技與開放的力量，改善社會問題與建構更美好的未來",
             'links' => [
             ],
@@ -96,7 +96,7 @@ function getAllSpeakers()
         'sam' => [
             'name' => '邱炫儒 (Sam)',
             'pic' => 'speaker/sam.jpg',
-            'title' => 'HTC 工程師',
+            'title' => 'HTC 技術副理',
             'bio' => "專注於 Android App 開發的持續整合流程並經常接觸開發維運相關的系統設計，目前服務於 HTC。",
             'links' => [
                 'Facebook' => 'https://zh-tw.facebook.com/iamsamchiu',
@@ -106,7 +106,7 @@ function getAllSpeakers()
             'name' => 'Akane Lee',
             'pic' => 'speaker/akane.jpg',
             'title' => 'Madders 創辦人',
-            'bio' => "嫁給 RD 的UI Designer",
+            'bio' => "嫁給 RD 的 UI Designer",
             'links' => [
                 'Facebook' => 'https://www.facebook.com/uiux.akane',
                 'Blog' => 'http://blog.akanelee.me/'
@@ -115,7 +115,7 @@ function getAllSpeakers()
         'yccsonar' => [
             'name' => '張逸中',
             'pic' => 'speaker/yccsonar.jpg',
-            'title' => '逸中軟體設計有限公司執行長',
+            'title' => '逸中軟體設計有限公司 執行長',
             'bio' => "大學畢業才開始寫程式的玩家，因為研究海底地形辨識開始接觸影像辨識，同時鑽研聲納科技，是目前國內唯一可以製作各式聲納軟體的怪咖！40 歲開始教程式設計，52 歲辭去大學教職投入業界，53 歲創業開設公司，主攻影像辨識技術，以在行動裝置中實現各種影像辨識技術為目標。",
             'links' => [
                 'Blog' => 'http://blog.udn.com/yccsonar/article',
@@ -124,7 +124,7 @@ function getAllSpeakers()
         'joe' => [
             'name' => '曾君宇 (魯蛇喬)',
             'pic' => 'speaker/joe.jpg',
-            'title' => '均一教育平台軟體工程師',
+            'title' => '均一教育平台 軟體工程師',
             'bio' => "魯蛇喬，不知道為什麼一直在寫 JavaScript 的 Tainan.py (Python) 社群聚會發起人。在告別了創業連續失敗家以及放假半年的魯蛇人生以後，目前在誠致教育基金會內擔任軟體工程師，希望讓國內的小朋友透過均一教育平台能夠有另一種學習的可能性。",
             'links' => [
             ],
