49c45c4fcf757bda1436a041d2829cd60370d411
Maple Huang <notemaple@yahoo.com.tw>
2016-08-21 23:33:01 +0800

    增加講者與社群的單頁

--- 2016/src/speaker.php
+++ 2016/src/speaker.php
@@ -3,5 +3,14 @@ function getAllSpeakers()
 {
     $jsonText = file_get_contents(__DIR__."/../api/speakers.json");
     $jsonArr = json_decode($jsonText,true);
-    return $jsonArr;
+    $returnArr = [];
+    foreach($jsonArr as $info) {
+        if($info['image']!='') {
+            $id = strtolower(pathinfo($info['image'],PATHINFO_FILENAME));
+        } else {
+            continue; //異常
+        }
+        $returnArr[$id] = $info;
+    }
+    return $returnArr;
 }
