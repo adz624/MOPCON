0a5971d9daf2d49f28f372df59f4de2f2332c2b7
Andy Tsai <bbandydd@gmail.com>
2017-08-12 18:08:06 +0800

    add barcode page

---
+++ 2017/qrcode.php
@@ -0,0 +1,17 @@
+<?php
+include __DIR__ . '/src/init.php';
+
+$pageid = 'qrcode';
+$params = [
+    'pageid' => $pageid,
+    'filemtime' => getLastUpdateTime($pageid), 
+    'heatmapRooms' => [],
+];
+
+$params['qrcodes'] = getScheduleMergeSpeaker();
+
+
+if (isset($_GET['api'])) {
+	getJson($params);
+}
+render('qrcode.twig', $params);
