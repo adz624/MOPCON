304be10c06d8fecc9e746edf627da91c1ffdf0bf
jackson <jacksonxu45252@gmail.com>
2017-08-17 02:40:53 +0800

    更新主辦社群頁面 修改贊助商等級名稱

--- 2017/community.php
+++ 2017/community.php
@@ -10,8 +10,12 @@ $params = [
     'community' => getAllCommunities(),
     'sponsors' => getAllSponsors(),
 ];
-if (isset($_GET['api'])) {
-	getJson($params);
-}
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id'])-1;
+    $params['community'] = getSingleCommunity($id);
+    // $params['og_image'] = $params['schedules']['pic'];
+    // $params['og_url'] = 'schedule.php?speaker=' . $speaker;
+    // $params['main']['ogdesc'] = $params['schedules']['title'] . " : " . $params['schedules']['summary'];
 
-render('community.twig', $params);
+}
+render(( isset($_GET['id']) )? 'communityDetail.twig' : 'community.twig', $params);
--- 2017/src/community.php
+++ 2017/src/community.php
@@ -11,3 +11,14 @@ function getAllCommunities()
 
     return getI18n($main);
 }
+function getSingleCommunity($id)
+{
+    $lang_zh = json_decode(file_get_contents("locales/community-zh_TW.json"), true);
+    $lang_en = json_decode(file_get_contents("locales/community-en.json"), true);
+    $main = [
+        'zh' => $lang_zh[$id],
+        'en' => $lang_en[$id],
+    ];
+
+    return getI18n($main);
+}
