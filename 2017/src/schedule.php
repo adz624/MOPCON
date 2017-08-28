fd5e68392e7a871609f7ac27e1f4a9138bf32d1e
jackson <jacksonxu45252@gmail.com>
2017-08-28 22:02:47 +0800

    增加欄位樣式判斷

--- 2017/src/schedule.php
+++ 2017/src/schedule.php
@@ -5,14 +5,17 @@ function getAllSchedule()
     $google_data = $google_data['feed']['entry'];
     $speakerDetail = getAllSpeakerDetail();
     $data = [];
-    $paintBG = ['休息', '午餐', '下午茶', 'End'];
+    $paintBG = ['休息 Break', '午餐 Lunch', '下午茶 Afternoon tea', 'End'];
 
     foreach ($google_data as $key => $item) {
+        // 跳過議會廳欄位
         if ($key === 0) {
             continue;
         }
-
+        // R1議程
         $content_day1 = [$item['gsx$_cokwr']['$t']];
+
+        // 如果同一時間點，其他廳有議程的話
         if (isset($item['gsx$_cre1l'])) {
             $content_day1[] = $item['gsx$_cpzh4']['$t'];
             $content_day1[] = $item['gsx$_cre1l']['$t'];
@@ -54,7 +57,9 @@ function getSpeakerDetail($speakers, $speakerDetail)
                     'schedule_topic' => $speakerDetail[$item]['schedule_topic'],
                     'name'           => $speakerDetail[$item]['name'],
                     'type'           => $speakerDetail[$item]['type'],
+                    'slide'          => $speakerDetail[$item]['slide'],
                     'room'           => count($speakers) === 1 ? 'All' : 'R' . ($key + 1),
+                    
                 ];
             } else {
                 $result[] = [
