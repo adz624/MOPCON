27aa3ff09b7778b98642d6301fbc8d10a43c5c8e
Hashman Lin <coosos810609@gmail.com>
2018-05-28 08:24:54 +0800

    fix 2018/index.php syntax error

--- 2018/index.php
+++ 2018/index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $params = [
     'pageid' => 'index',
 ];
-MopCon2018\Utils\Base::render('components/index.twig', $params ?? []);
+MopCon2018\Utils\Base::render('components/index.twig', $params);
