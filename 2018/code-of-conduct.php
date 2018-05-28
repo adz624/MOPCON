0452ee04b9dca7734d688e12de0b4bef4f6de7c4
Hashman Lin <coosos810609@gmail.com>
2018-05-28 08:23:36 +0800

    fix syntax error

--- 2018/code-of-conduct.php
+++ 2018/code-of-conduct.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $params = [
     'pageid' => 'code-of-conduct',
 ];
-MopCon2018\Utils\Base::render('components/code-of-conduct.twig', $params ?? []);
+MopCon2018\Utils\Base::render('components/code-of-conduct.twig', $params);
