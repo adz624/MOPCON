9c65daf53d166c051f6bafbf381e5430f620eaf0
Webb <u.master.o.twn@gmail.com>
2018-06-01 02:32:18 +0800

    updated code-of-conduct i18n and fit syntax to php5
    This reverts commit a15324de5bbf32d1d4109422aac62674b25c1e99.

--- 2018/code-of-conduct.php
+++ 2018/code-of-conduct.php
@@ -1,7 +1,25 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$spreadSheetKey = '1v2Cdv40jhN7ekWANdSm9Fsv6rsGjN_VGh2qThjlfvp0';
+$columnMapping = [
+    'title' => 'title',
+    'content' => 'content',
+    'en_title' => 'en-title',
+    'en_content' => 'en-content',
+];
+
+$apiData = new \MopCon2018\Utils\GoogleDocsSpreadsheet($spreadSheetKey, $columnMapping);
+
+// var_dump($apiData->toArray()[0]);
+$lang = \MopCon2018\Utils\Base::getLang();
+
 $params = [
     'pageid' => 'code_of_conduct',
+    'body' => [
+        'code_of_conduct' => $lang == 'zh'
+        ? $apiData->toArray()[0]['content']
+        : $apiData->toArray()[0][$lang . '_content'],
+    ],
 ];
-MopCon2018\Utils\Base::render('components/code-of-conduct.twig', $params);
+\MopCon2018\Utils\Base::render('components/code-of-conduct.twig', $params);
