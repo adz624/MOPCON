419e81432939316ce0762cd04a12973c7900b70a
Webb <u.master.o.twn@gmail.com>
2018-06-23 00:00:59 +0800

    更新 Google Sheet API Fetch Class; 增加了 gridid 的參數

--- 2018/src/Utils/GoogleDocsSpreadsheet.php
+++ 2018/src/Utils/GoogleDocsSpreadsheet.php
@@ -4,16 +4,18 @@ namespace MopCon2018\Utils;
 class GoogleDocsSpreadsheet
 {
     private $key;
+    private $gridId;
     private $uri;
     private $raw;
     private $rawObj;
     private $data;
     private $rows;
 
-    public function __construct($key, $columns = [])
+    public function __construct($key, $columns = [], $gridId = 'od6')
     {
+        $this->gridId = $gridId;
         $this->key = $key;
-        $this->uri = 'https://spreadsheets.google.com/feeds/list/' . $this->key . '/od6/public/values?alt=json';
+        $this->uri = "https://spreadsheets.google.com/feeds/list/$this->key/$this->gridId/public/values?alt=json";
         $this->raw = file_get_contents($this->uri);
         $this->rawObj = json_decode($this->raw);
         $this->rows = $this->rawObj->feed->entry;
@@ -26,9 +28,9 @@ class GoogleDocsSpreadsheet
         return $this->data;
     }
 
-    public function toJSON()
+    public function toJSON($option = 0)
     {
-        return json_encode($this->data);
+        return json_encode($this->data, $option);
     }
 
     private function getBody()
