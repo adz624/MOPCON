513311211aa96884453b00573eff1b74d2d34740
Webb <u.master.o.twn@gmail.com>
2018-06-23 22:34:50 +0800

    update routes and add api info

--- 2018/api/src/bootstrap/app.php
+++ 2018/api/src/bootstrap/app.php
@@ -8,8 +8,27 @@ $container['ApiController'] = function ($container) {
     return new MopConApi2018\App\Http\ApiController($container);
 };
 
-$app->get('/2018/api/{routes:.*}', 'ApiController');
+$app->get('/2018/api/__info__', function () {
+    try {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://hackmd.io/s/ByvLG0oWX");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TCP_KEEPALIVE, 1);
+        curl_setopt($ch, CURLOPT_TCP_KEEPIDLE, 2);
+        $data = curl_exec($ch);
+        if (curl_errno($ch)) {
+            throw new Exception(curl_error($ch));
+        }
+        curl_close($ch);
+        echo $data;
+    } catch (Exception $e) {
+        echo '>_____________________<';
+    }
+});
 
-$app->add(new MopConApi2018\App\Http\ApiMiddleware($container));
+// 用 group 可以從 global 分離出來
+$app->group('/2018/api', function () {
+    $this->any('/{routes:.*}', 'ApiController');
+})->add(new MopConApi2018\App\Http\ApiMiddleware($container));
 
 $app->run();
