8009ec01710ca4c0d160656e19df1d012d265496
jackson <jacksonxu45252@gmail.com>
2018-08-30 04:19:09 +0800

    新增 ticket 頁面, 添加語系檔

---
+++ 2018/ticket.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+
+
+// var_dump($apiData->toArray()[0]);
+$lang = \MopCon2018\Utils\Base::getLang();
+
+$params = [
+    
+];
+\MopCon2018\Utils\Base::render('components/ticket.twig', $params);
