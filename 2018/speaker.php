4c9de7a39afb1d226043ce866569763ae4ab3ed1
Cloudfun-Jackson <jackson@cloudfun.com.tw>
2018-09-04 10:24:57 +0800

    修改 API 路徑

--- 2018/speaker.php
+++ 2018/speaker.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$apiData = json_decode(file_get_contents('https://dev.mopcon.org/2018/api/speaker'), true);
+$apiData = json_decode(file_get_contents('https://mopcon.org/2018/api/speaker'), true);
 $apiData = $apiData['payload'];
 
 // var_dump($apiData->toArray()[0]);
