ad506271f75a22a67e212926bc05ba0c0f004f95
Hashman Lin <coosos810609@gmail.com>
2018-09-05 10:46:41 +0800

    因預算調整，調整票種資訊

--- 2018/ticket.php
+++ 2018/ticket.php
@@ -1,14 +1,12 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
-
+use MopCon2018\Utils\Base;
 
 // var_dump($apiData->toArray()[0]);
 $lang = \MopCon2018\Utils\Base::getLang();
 
 $params = [
     'pageid' => 'ticket',
-    
 ];
-//\MopCon2018\Utils\Base::render('components/ticket.twig', $params);
+Base::render('components/ticket.twig', $params);
