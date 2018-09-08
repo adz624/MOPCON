d64718a0ec0ffa93f0e7f19212f18de7e2fb4e4c
jackson <jacksonxu45252@gmail.com>
2018-09-08 12:39:24 +0800

    新增 暖身場頁面

---
+++ 2018/warm-up.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use MopCon2018\Utils\Base;
+
+// var_dump($apiData->toArray()[0]);
+$lang = \MopCon2018\Utils\Base::getLang();
+
+$params = [
+    'pageid' => 'warm-up',
+];
+Base::render('components/warm_up.twig', $params);
