ed13f15626f945bc0b5e5f1503f4c003c80c0bcf
HashLin <coosos810609@gmail.com>
2018-10-25 11:46:32 +0800

    Change community to redis

--- 2018/api/src/app/Http/ApiController.php
+++ 2018/api/src/app/Http/ApiController.php
@@ -214,23 +214,28 @@ class ApiController extends Controller
 
     private function accessCommunity($request, $response, $args)
     {
-        $apiData = $this->cache->refreshIfExpired($this->resourceName, function () {
-            $apiData = new GoogleDocsSpreadsheet(
-                $this->resource['sheetKey'],
-                $this->resource['columns'],
-                $this->resource['sheetGridId']
-            );
+        $redis_key = $this->getRedisKey('community');
+        $redis_data = $this->redis->get($redis_key);
+        if ($redis_data) {
+            return $response = $response->withJson(json_decode($redis_data, true), 200, $this->jsonOptions);
+        }
 
-            $apiDataArray = $apiData->toArray();
+        $apiData = new GoogleDocsSpreadsheet(
+            $this->resource['sheetKey'],
+            $this->resource['columns'],
+            $this->resource['sheetGridId']
+        );
 
-            foreach ($apiDataArray as $key => &$value) {
-                if (!empty($value['logo'])) {
-                    $value['logo'] = $this->fullUrlToAssets . '/images/community/' . $value['logo'];
-                }
+        $apiDataArray = $apiData->toArray();
+
+        foreach ($apiDataArray as $key => &$value) {
+            if (!empty($value['logo'])) {
+                $value['logo'] = $this->fullUrlToAssets . '/images/community/' . $value['logo'];
             }
+        }
 
-            return ['payload' => $apiDataArray];
-        }, $this->globalCacheSeconds);
+        $apiData = ['payload' => $apiDataArray];
+        $this->redis->setex($redis_key, 600, json_encode($apiData));
 
         return $response = $response->withJson($apiData, 200, $this->jsonOptions);
     }
