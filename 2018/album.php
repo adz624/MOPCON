4805d5b1c61245d2f72790d55b042924d503f863
Jackson <jacksonxu45252@gmail.com>
2019-01-03 21:07:44 +0800

    新增 相簿json檔＆相簿產生程式

--- 2018/album.php
+++ 2018/album.php
@@ -1,9 +1,18 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+function getAlbum()
+{
+    $jsonText = file_get_contents(__DIR__ . "/locales/album/photodata.json");
+    $jsonArr = json_decode($jsonText, true);
+    $returnArr = $jsonArr;
 
+    
+    return $returnArr;
+}
 $params = [
     'pageid' => 'album',
     'pagetitle' => '相簿',
+    'album' => getAlbum()
     // 'i18n' => file_get_contents(__DIR__ . )
 ];
 
