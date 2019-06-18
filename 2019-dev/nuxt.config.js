f0e0ac313fe8fd6c75c91aa8da397e6c3769a5ec
hsimao <e087754958@gmail.com>
2019-06-18 20:31:09 +0800

    add: base style setting, head meta setting
    add: setting bg color

--- 2019-dev/nuxt.config.js
+++ 2019-dev/nuxt.config.js
@@ -1,5 +1,4 @@
-const pkg = require('./package')
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   mode: 'universal',
@@ -8,13 +7,98 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    // 多語系
+    htmlAttrs: {
+      lang: 'zh-Hant',
+    },
+    title: '行動科技年會 2019 | Mobile / Open / Platform Conference',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0',
+      },
+      { 'http-equiv': 'X-UA-Compatible', content: 'ie=edge' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '行動科技年會 2019 | Mobile / Open / Platform Conference',
+      },
+      // fb seo
+      {
+        hid: 'og-title',
+        property: 'og:title',
+        content: '行動科技年會 2019 | Mobile / Open / Platform Conference',
+      },
+      {
+        hid: 'og-description',
+        property: 'og:description',
+        content: '行動科技年會 2019 | Mobile / Open / Platform Conference',
+      },
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og-site_name',
+        property: 'og:site_name',
+        content: '行動科技年會 2019 | Mobile / Open / Platform Conference',
+      },
+      {
+        hid: 'og-url',
+        property: 'og:url',
+        content: 'https://mopcon.org/2019/',
+      },
+      {
+        hid: 'og-image',
+        property: 'og:image',
+        content: 'https://mopcon.org/2019/banner.png',
+      },
+      { hid: 'og-locale', property: 'og:locale', content: 'zh_tw' },
+      // twitter seo
+      { hid: 'twitter-card', name: 'twitter:card', content: 'app' },
+      {
+        hid: 'twitter-site',
+        name: 'twitter:site',
+        content: '行動科技年會 2019 | Mobile / Open / Platform Conference',
+      },
+      {
+        hid: 'twitter-description',
+        name: 'twitter:description',
+        content: '行動科技年會 2019 | Mobile / Open / Platform Conference',
+      },
+      {
+        hid: 'twitter-app:name:iphone',
+        name: 'twitter:app:name:iphone',
+        content: '行動科技年會 2019 | Mobile / Open / Platform Conference',
+      },
+      {
+        hid: 'twitter-app:name:ipad',
+        name: 'twitter:app:name:ipad',
+        content: '行動科技年會 2019 | Mobile / Open / Platform Conference',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // 引入 google font 字體
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Noto+Sans+TC:400,500&display=swap&subset=chinese-traditional',
+      },
+      // 多語系 seo
+      {
+        rel: 'alternate',
+        hreflang: 'z-default',
+        href: 'https://mopcon.org/2019/',
+      },
+      {
+        rel: 'alternate',
+        hreflang: 'zh-Hant-TW',
+        href: 'https://mopcon.org/2019/zh',
+      },
+      {
+        rel: 'alternate',
+        hreflang: 'en-us',
+        href: 'https://mopcon.org/2019/en',
+      },
+    ],
   },
 
   /*
@@ -25,7 +109,7 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~assets/styles/normalize.css', '~assets/styles/global.scss'],
 
   /*
    ** Plugins to load before mounting the App
@@ -38,7 +122,13 @@ module.exports = {
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
+    // 全域 sass 變數設定
+    '@nuxtjs/style-resources',
   ],
+  // 全域 sass (變數、mixin、extend... ) 檔案引入設定
+  styleResources: {
+    scss: ['./assets/styles/variables.scss'],
+  },
   /*
    ** Axios module configuration
    */
@@ -55,8 +145,6 @@ module.exports = {
      */
     extend(config, ctx) {
       if (!ctx.isDev) {
-        // config.output.publicPath = './_nuxt/'
-        // config.output.publicPath = path.resolve(__dirname, '/2019/_nuxt/')
       }
     },
   },
@@ -68,4 +156,4 @@ module.exports = {
     dir: path.resolve(__dirname, '../2019/'),
     // dir: '../2019',
   },
-}
+};
