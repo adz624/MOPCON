abb2903a4820f576d06f72016673f670dc3bfbeb
hsimao <e087754958@gmail.com>
2019-06-29 23:51:54 +0800

    edit: 調整首頁Section擺放位置

---
+++ 2019-dev/pages/home/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="index">
+        <Navbar />
+
+        <SectionHero id="sectionHero" />
+
+        <!-- section image wall -->
+        <SectionImageWall id="sectionImageWall" />
+
+        <!-- section about -->
+        <SectionAbout id="sectionAbout" />
+
+        <!-- section place -->
+        <SectionPlace id="sectionPlace" />
+
+        <!-- section 贊助區塊 -->
+        <SectionSponsor id="sectionSponsor" />
+
+        <!-- section fb 動態牆 -->
+        <SectionFbNews id="sectionFbNews" />
+
+        <!-- section 歷年 MOPCON -->
+        <SectionPastYears id="sectionPastYears" />
+    </div>
+</template>
+
+<script>
+import Navbar from "~/components/Navbar";
+import SectionHero from "./SectionHero";
+import SectionImageWall from "./SectionImageWall";
+import SectionAbout from "./SectionAbout";
+import SectionPlace from "./SectionPlace";
+import SectionSponsor from "./SectionSponsor";
+import SectionFbNews from "./SectionFbNews";
+import SectionPastYears from "./SectionPastYears";
+
+export default {
+    name: "pageIndex",
+    components: {
+        Navbar,
+        SectionHero,
+        SectionImageWall,
+        SectionAbout,
+        SectionPlace,
+        SectionSponsor,
+        SectionFbNews,
+        SectionPastYears
+    }
+};
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
