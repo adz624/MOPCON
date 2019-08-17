c76b0bd51b5eab98d0269cadab36667bb1639edf
hsimao <e087754958@gmail.com>
2019-08-17 13:10:17 +0800

    add: 主、交流議程頁面, 共用 banner、appSection 樣式 #212

---
+++ 2019-dev/components/SectionApp/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="sectionApp">
+        <div class="sectionApp__content">
+            <div class="sectionApp__info">
+                <h3 class="sectionApp__info__title">怕錯過感興趣的講者議程嗎？</h3>
+                <div class="sectionApp__info__text">下載 MOPCON App，建立你的行事曆吧！</div>
+                <div class="sectionApp__info__downloads">
+                    <a class="google-play" target="_blank" href="https://play.google.com/store/apps/details?id=org.mopcon.session.app"></a>
+                    <a class="app-store" target="_blank" href="https://apps.apple.com/tw/app/mopcon/id721411970"></a>
+                </div>
+            </div>
+            <div class="sectionApp__phone"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'sectionApp',
+};
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
---
+++ 2019-dev/pages/schedule/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="schedule">
+        <div class="schedule__banner"></div>
+        <div class="schedule__content">
+        </div>
+        <SectionApp />
+    </div>
+</template>
+
+<script>
+import SectionApp from '~/components/SectionApp';
+
+export default {
+    name: 'schedule',
+    components: {
+        SectionApp,
+    },
+};
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
