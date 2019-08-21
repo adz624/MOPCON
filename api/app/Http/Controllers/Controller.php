961958c66c4f8a4ac48e360c38993258a2eec3d2
方威(Eric) <ree0916tw@gmail.com>
2019-08-22 02:28:52 +0800

    add: 議程 api 實作

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -9,7 +9,7 @@ class Controller extends BaseController
 {
     protected $function = null;
     protected $jsonAry;
-    private $path = __DIR__ . '/../../../resource/assets/json/';
+    protected $path = __DIR__ . '/../../../resource/assets/json/';
 
     public function __construct()
     {
