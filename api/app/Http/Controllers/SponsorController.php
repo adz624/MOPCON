61c29f6e3544f40a0abd84357289d5ebf0e5c042
Hash Lin <coosos810609@gmail.com>
2019-08-25 14:52:15 +0800

    Refactor SponsorController and fix coding style

--- api/app/Http/Controllers/SponsorController.php
+++ api/app/Http/Controllers/SponsorController.php
@@ -7,7 +7,10 @@ use Illuminate\Http\Request;
 class SponsorController extends Controller
 {
     use ApiTrait;
+
     private $sponsorTypes = ['tony_stark', 'bruce_wayne', 'hacker', 'geek', 'developer', 'special_cooperation', 'special_thanks'];
+    protected $function = 'sponsor';
+
     /**
      * Get all or specific Sponsors information.
      *
@@ -15,13 +18,11 @@ class SponsorController extends Controller
      */
     public function index(Request $request)
     {
-        $env = env('APP_ENV') ? env('APP_ENV') : 'production';
-        $sponsorfilePath = __DIR__ . '/../../../resource/assets/json/' . ($env == 'production' ? 'sponsor.json' : 'sponsor-dev.json') ;
-        $sponsorJson = json_decode(file_get_contents($sponsorfilePath), true);
         $sponsorIds = $request->get('sponsor_id', '');
         $sponsorIdArr = $sponsorIds == '' ? [] : explode(",", $sponsorIds);
-        return count($sponsorIdArr) ? $this->getSpecificSponsors($sponsorJson, $sponsorIdArr) : $this->getAllSponsors($sponsorJson);
+        return count($sponsorIdArr) ? $this->getSpecificSponsors($this->jsonAry, $sponsorIdArr) : $this->getAllSponsors($this->jsonAry);
     }
+
     private function getAllSponsors($sponsorJson)
     {
         $data = [
@@ -39,6 +40,7 @@ class SponsorController extends Controller
         }
         return $this->returnSuccess('success', $data);
     }
+
     private function getSpecificSponsors($sponsorJson, $sponsorIdArr)
     {
         $data = [];
