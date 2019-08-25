b682f7095abaefc8921b3b1cf6da7b7457d28154
Hash Lin <coosos810609@gmail.com>
2019-08-25 15:36:16 +0800

    Refactor SponsorController get speaker information by speaker.json

--- api/app/Http/Controllers/SponsorController.php
+++ api/app/Http/Controllers/SponsorController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class SponsorController extends Controller
 {
     use ApiTrait;
 
-    private $sponsorTypes = ['tony_stark', 'bruce_wayne', 'hacker', 'geek', 'developer', 'special_cooperation', 'special_thanks'];
+    private $sponsorTypes = ['tony_stark', 'bruce_wayne', 'hacker', 'geek', 'developer', 'special_cooperation', 'special_thanks', 'co-organizer', 'ksg_support'];
+    private $speakerAry = [];
     protected $function = 'sponsor';
 
     /**
@@ -20,6 +22,9 @@ class SponsorController extends Controller
     {
         $sponsorIds = $request->get('sponsor_id', '');
         $sponsorIdArr = $sponsorIds == '' ? [] : explode(",", $sponsorIds);
+        $speakerFilePath = env('APP_ENV') === 'production' ? 'speaker.json' : 'speaker-dev.json';
+        $this->speakerAry = json_decode(file_get_contents($this->path . $speakerFilePath), true);
+
         return count($sponsorIdArr) ? $this->getSpecificSponsors($this->jsonAry, $sponsorIdArr) : $this->getAllSponsors($this->jsonAry);
     }
 
@@ -33,10 +38,12 @@ class SponsorController extends Controller
             'developer' => [],
             'special_cooperation' => [],
             'special_thanks' => [],
-            'education' => []
+            'education' => [],
+            'co-organizer' => [],
+            'ksg_support' => [],
         ];
         foreach ($sponsorJson as $index => $sponsor) {
-            array_push($data[$sponsor['sponsor_type']], $sponsor);
+            array_push($data[$sponsor['sponsor_type']], $this->extractor($sponsor));
         }
         return $this->returnSuccess('success', $data);
     }
@@ -46,12 +53,49 @@ class SponsorController extends Controller
         $data = [];
         foreach ($sponsorJson as $index => $sponsor) {
             if (in_array($sponsor['sponsor_id'], $sponsorIdArr)) {
-                array_push($data, $sponsor);
+                array_push($data, $this->extractor($sponsor));
             }
         }
         if (count($data) == 0) {
             return $this->returnNotFoundError();
         }
         return $this->returnSuccess('success', $data);
+    }
+
+    private function extractor($sponsor)
+    {
+        $sponsor['speaker_information'] = [];
+        foreach ($this->speakerAry as $speaker) {
+            if ((int) $speaker['sponsor_id'] === (int) $sponsor['sponsor_id']) {
+                $start = Carbon::createFromTimestamp($speaker['started_at'], 'Asia/Taipei');
+                $end = Carbon::createFromTimestamp($speaker['ended_at'], 'Asia/Taipei');
+
+                $sponsor['speaker_information'][] = [
+                    'speaker_img' => $speaker['photo_for_sponsor_web'],
+                    'img' => [
+                        'mobile' => $speaker['photo_for_sponsor_mobile'],
+                        'web' => $speaker['photo_for_sponsor_web'], // extra
+                    ],
+                    'speaker_name' => $speaker['name'],
+                    'name' => $speaker['name'],
+                    'speaker_name_en' => $speaker['name_e'],
+                    'name_en' => $speaker['name_e'],
+                    'speaker_title' => $speaker['job_title'],
+                    'title' => $speaker['job_title'],
+                    'speaker_title_en' => $speaker['job_title_e'], // extra
+                    'title_en' => $speaker['job_title_e'], // extra
+                    'speaker_date' => $start->format('m/d'),
+                    'speaker_duration' => $start->hour . ':' . $start->minute . ' - ' . $end->hour . ':' . $end->minute,
+                    'started_at' => $speaker['started_at'],
+                    'endeded_at' => $speaker['ended_at'],
+                    'speaker_room' => $speaker['room'],
+                    'room' => $speaker['room'],
+                    'speaker_keywords' => $speaker['tags'],
+                    'tags' => $speaker['tags'],
+                ];
+            }
+        }
+
+        return $sponsor;
     }
 }
