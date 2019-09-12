9b64bb1ae6044771a29f3a55d1dd889bc782f5ab
Tzuhui <angel1996421@gmail.com>
2019-09-12 20:30:41 +0800

    新增贊助商頁面

---
+++ 2019-dev/pages/sponsor/SectionSponsor/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="section section-sponsor">
+        <div class="section-sponsor__content">
+            <div class="people-3"></div>
+            <div class="people-4"></div>
+            <div class="people-5"></div>
+            <div class="people-6"></div>
+            <div class="people-7"></div>
+
+            <div class="light">
+                <div class="light__head"></div>
+            </div>
+            <h2 class="title">{ 贊助商<span class="color-primary">招募中</span>; }</h2>
+            <h3 class="subtitle">成為全台最大行動科技領域社群研討會的參與者及貢獻者之一</h3>
+            <Btn>
+                <a :href="pdfLink" target="_blank">贊助企劃書</a>
+            </Btn>
+            <p class="email" v-clipboard:copy="email"
+                v-clipboard:success="onCopy"
+                v-clipboard:error="onError">sponsor@mopcon.org</p>
+            <p class="text">MOPCON 2019 贊助組 收</p>
+            <div class="line"></div>
+        </div>
+    </section>
+</template>
+
+<script>
+import Btn from "~/components/Btn";
+
+export default {
+    name: "sectionSponsor",
+    components: {
+        Btn
+    },
+    data() {
+        return {
+            email: "sponsor@mopcon.org",
+            pdfLink:
+                "https://drive.google.com/open?id=1fbjbdAOrs9hGy3r-DkMnyNl5eWgYJsKd"
+        };
+    },
+    methods: {
+        onCopy() {
+            alert(`信箱 ${this.email} 複製成功`);
+        },
+        onError: function(e) {
+            alert(`信箱 ${this.email} 複製失敗`);
+        }
+    }
+};
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
---
+++ 2019-dev/pages/sponsor/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="sponsor">
+    <div class="sponsor__banner"></div>
+    <div class="section">
+      <img class="coin" src="./images/coin.png" alt="">
+      <h2 class="title"><span>{ 我們感謝</span><span class="color-primary"><br class="break">過往支持 MOPCON 的贊助商</span><span>;
+          }</span></h2>
+      <p class="intro">MOPCON 目前已是南台灣最大技術社群研討會，成立宗旨為<span
+          class="color-secondary">連結產業與資訊工程技術，以培養更多資訊科技人才，推動高雄產業創新發展</span>，而沒有夥伴們的支持，這個願景就不可能成真！謝謝這 7
+        年來每一位夥伴的加入，和我們一起凝聚南臺灣的人才和知識，共同成長、學習！</p>
+      <a href="https://drive.google.com/open?id=1fbjbdAOrs9hGy3r-DkMnyNl5eWgYJsKd" target="_blank" class="btn-primary">成為我們的夥伴</a>
+      <div class="slider sponsor_carrousel">
+        <div class="slider-navigation">
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </div>
+        <div v-swiper:mySwiper="swiperOption" class="slider-container">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide" v-for="n in 10" :style="`background-image: url(${require(`./images/sponsor_carrousel/sponsor_carrousel_${n}.png`)})`"></div>
+          </div>
+        </div>
+      </div>
+      <SectionWantToJoin />
+      <SectionReward />
+      <SectionPartner />
+      <SectionSponsor />
+    </div>
+  </div>
+</template>
+
+<script>
+  import SectionWantToJoin from './SectionWantToJoin';
+  import SectionReward from './SectionReward';
+  import SectionPartner from './SectionPartner';
+  import SectionSponsor from './SectionSponsor';
+
+  export default {
+    name: 'sponsor',
+    components: {
+      SectionWantToJoin,
+      SectionReward,
+      SectionPartner,
+      SectionSponsor,
+    },
+    data() {
+      return {
+        swiperOption: {
+          effect: 'coverflow',
+          grabCursor: true,
+          loop: true,
+          centeredSlides: true,
+          slidesPerView: 'auto',
+          coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }
+        }
+      }
+    }
+  };
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
