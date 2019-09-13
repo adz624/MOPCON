06b1d56aed70cc8f986cdbd86b281bd7d9659e9c
張景雲 <z5254215560@gmail.com>
2019-09-13 17:34:53 +0800

    Add redis for facebook api

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -23,9 +23,8 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
-
-// $app->withEloquent();
+$app->withFacades();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -80,7 +79,7 @@ $app->configure('logging');
 | totally optional, so you are not required to uncomment this line.
 |
 */
-
+$app->register(Illuminate\Redis\RedisServiceProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
