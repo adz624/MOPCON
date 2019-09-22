c553fef33919a4813021a2934e995fff42077d9d
hsimao <e087754958@gmail.com>
2019-09-22 12:25:00 +0800

    add: 新增懸浮購票按鈕

---
+++ 2019-dev/components/BtnBuy/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <a class="btnBuy" :href="link">
+        <div class="btnBuy__text">
+            我要<br>購票
+        </div>
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'btnBuy',
+    props: {
+        link: {
+            type: String,
+            default: 'ticket',
+        },
+    },
+};
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
