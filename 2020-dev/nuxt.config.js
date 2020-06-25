1fe2a04e88541c50c030530b269e4d3a093905e4
TzuHui <angel1996421@gmail.com>
2020-06-25 16:21:40 +0800

    add: 2020-page

---
+++ 2020-dev/nuxt.config.js
@@ -0,0 +1,91 @@
+const path = require('path')
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    htmlAttrs: {
+      lang: 'zh-Hant'
+    },
+    title: '行動科技年會 2020 | Mobile / Open / Platform Conference',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'ie=edge' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~assets/styles/all.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/tailwindcss'
+  ],
+  styleResources: {
+    scss: ['./assets/styles/variables.scss']
+  },
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  router: {
+    // base: '/2020/',
+    base: path.resolve(__dirname, '/2020/')
+  },
+  generate: {
+    dir: path.resolve(__dirname, '../2020/')
+  }
+}
