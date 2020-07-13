c4a449416a7b06dbdd6e5942f92dcc777ee276b4
TzuHui <angel1996421@gmail.com>
2020-07-14 01:06:09 +0800

    調整 2020 官網 meta

--- 2020-dev/nuxt.config.js
+++ 2020-dev/nuxt.config.js
@@ -11,12 +11,59 @@ module.exports = {
     title: '行動科技年會 2020 | Mobile / Open / Platform Conference',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0' },
       { 'http-equiv': 'X-UA-Compatible', content: 'ie=edge' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'MOPCON 2020 堅持濁水溪以南，南台灣最大行動科技年會，10/19-10/20 高雄國際會議中心與您見面。持續匯集知識與人才，打造高速資訊交流圈，提升個人與團隊價值，讓知識影響正向循環。' },
+      // fb seo
+      {
+        hid: 'og-title',
+        property: 'og:title',
+        content: '行動科技年會 2020 | Mobile / Open / Platform Conference'
+      },
+      {
+        hid: 'og-description',
+        property: 'og:description',
+        content:
+          'MOPCON 2020 堅持濁水溪以南，南台灣最大行動科技年會，10/24 -10/25 高雄國際會議中心與您見面。持續匯集知識與人才，打造高速資訊交流圈，提升個人與團隊價值，讓知識影響正向循環。'
+      },
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og-site_name',
+        property: 'og:site_name',
+        content: '行動科技年會 2020 | Mobile / Open / Platform Conference'
+      },
+      {
+        hid: 'og-url',
+        property: 'og:url',
+        content: 'https://mopcon.org/2020/'
+      },
+      { hid: 'og-locale', property: 'og:locale', content: 'zh_tw' },
+      // twitter seo
+      { hid: 'twitter-card', name: 'twitter:card', content: 'app' },
+      {
+        hid: 'twitter-site',
+        name: 'twitter:site',
+        content: '行動科技年會 2020 | Mobile / Open / Platform Conference'
+      },
+      {
+        hid: 'twitter-description',
+        name: 'twitter:description',
+        content:
+          'MOPCON 2020 堅持濁水溪以南，南台灣最大行動科技年會，10/19-10/20 高雄國際會議中心與您見面。持續匯集知識與人才，打造高速資訊交流圈，提升個人與團隊價值，讓知識影響正向循環。'
+      },
+      {
+        hid: 'twitter-app:name:iphone',
+        name: 'twitter:app:name:iphone',
+        content: '行動科技年會 2020 | Mobile / Open / Platform Conference'
+      },
+      {
+        hid: 'twitter-app:name:ipad',
+        name: 'twitter:app:name:ipad',
+        content: '行動科技年會 2020 | Mobile / Open / Platform Conference'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/2020/favicon.ico' }
     ]
   },
   /*
