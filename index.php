34febc0a0af6bbfcf064268916fd8c397eee4d4e
方威(Eric) <ree0916tw@gmail.com>
2020-07-29 19:38:29 +0800

    Revert "mod : [暫時]預設進入跳過 2020"
    This reverts commit 0e454efea1d91ebfb7cb1596a8d87cbe335bc1a3.

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 date_default_timezone_set("Asia/Taipei");
 for ($i = date("Y"); $i >= 2012; $i--) {
-	if ($i == 2020) {
-		continue;
-	}
 	if (file_exists ( __DIR__.'/'.$i)) {
 		header('Location: ./'.$i.'/');
 		exit;
