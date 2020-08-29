4f86466035022c230c87bcfae241ecbef10137fd
TzuHui <angel1996421@gmail.com>
2020-08-30 01:29:33 +0800

    新增售票頁面

---
+++ 2020-dev/pages/ticket/ticketCard/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <a :href="ticketData.link" :class="[ticketData.class.bg, ticketData.class.text, ticketData.class.cardBg, {'disabled': ticketData.status !== 1}]" class="card-ticket rounded-md px-6 py-4 flex flex-col relative overflow-hidden lg:mb-0 mb-4 md:mx-0 mx-6" style="min-height: 350px" target="_blank">
+    <div :class="{'mb-24': ticketData.id < 5}">
+      <div class="flex justify-between items-center flex-wrap">
+        <h4 class="text-xl font-bold">
+          {{ ticketData.title }}
+        </h4>
+        <p>限量 {{ ticketData.count }} 名</p>
+      </div>
+      <ul class="mt-4">
+        <li v-for="goods in ticketData.lists" :key="goods" class="checkmark mb-2">
+          {{ goods }}
+        </li>
+      </ul>
+      <p class="mt-6">
+        {{ ticketData.description }}
+      </p>
+    </div>
+    <button class="btn mt-auto mx-auto" :class="ticketData.class.btn" disabled>
+      {{ ticketStatusText(ticketData.status, ticketData.price) }}
+    </button>
+  </a>
+</template>
+
+<script>
+
+export default {
+  props: {
+    ticketData: {
+      type: Object,
+      default () {
+        return { message: 'hello' }
+      }
+    }
+  },
+  methods: {
+    ticketStatusText (status, price = '') {
+      if (status === 0) {
+        return '敬請期待'
+      } else if (status === 1) {
+        return `NT$ ${price}`
+      } else if (status === 2) {
+        return '已完售'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped>
+
+</style>
