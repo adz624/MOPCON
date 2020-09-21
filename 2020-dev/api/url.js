e9d2bb75772740a28586bf5fd222b1e3aae48f9f
TzuHui <angel1996421@gmail.com>
2020-09-22 00:15:41 +0800

    新增贊助商頁面

--- 2020-dev/api/url.js
+++ 2020-dev/api/url.js
@@ -2,3 +2,5 @@ export const fbNews = '/api/2020/facebook/posts'
 
 export const speaker = '/api/2020/speaker'
 export const tags = '/api/2020/speaker/tags'
+
+export const sponsor = '/api/2020/sponsor'
