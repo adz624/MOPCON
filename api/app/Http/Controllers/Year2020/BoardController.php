e472c8fe5b1c03244228a7d88935d7004227142d
unciax <unciaq@gmail.com>
2020-10-05 23:35:45 +0800

    add: 2020 board controller

---
+++ api/app/Http/Controllers/Year2020/BoardController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Year2020;
+
+use App\Http\Controllers\BaseBoardController;
+
+class BoardController extends BaseBoardController
+{
+    protected $year = 2020;
+}
