60b61501d968dd459a9a637eec239efa35d97a76
方威(Eric) <ree0916tw@gmail.com>
2020-10-26 23:04:34 +0800

    remove fb session

--- 2019-dev/pages/home/index.vue
+++ 2019-dev/pages/home/index.vue
@@ -17,9 +17,6 @@
         <!-- section 核心贊助商夥伴 -->
         <SectionSponsorCore id="sectionSponsorCore" />
 
-        <!-- section fb 動態牆 -->
-        <SectionFbNews id="sectionFbNews" />
-
         <!-- section 歷年 MOPCON -->
         <SectionPastYears id="sectionPastYears" />
 
@@ -38,7 +35,6 @@ import SectionSpeaker from './SectionSpeaker';
 import SectionAbout from './SectionAbout';
 import SectionPlace from './SectionPlace';
 import SectionSponsorCore from './SectionSponsorCore';
-import SectionFbNews from './SectionFbNews';
 import SectionPastYears from './SectionPastYears';
 import SectionBuy from './SectionBuy';
 
@@ -57,7 +53,6 @@ export default {
         SectionAbout,
         SectionPlace,
         SectionSponsorCore,
-        SectionFbNews,
         SectionPastYears,
         SectionBuy,
     },
