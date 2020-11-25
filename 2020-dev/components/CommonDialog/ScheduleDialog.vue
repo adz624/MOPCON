b75d56c03d187b44a9cc1f4232d35c8afcc28542
TzuHui <angel1996421@gmail.com>
2020-11-25 22:26:16 +0800

    新增簡報 Button

--- 2020-dev/components/CommonDialog/ScheduleDialog.vue
+++ 2020-dev/components/CommonDialog/ScheduleDialog.vue
@@ -29,7 +29,10 @@
       </section>
     </div>
     <div slot="footer">
-      <share-btn :btn-list-show.sync="btnListShow" :share-url="shareUrl" />
+      <div class="flex">
+        <a v-if="speaker && speaker.link_slide" class="btn-ppt mr-4 text-center" :href="speaker? speaker.link_slide: '#'" target="_blank">簡報連結</a>
+        <share-btn :btn-list-show.sync="btnListShow" :share-url="shareUrl" />
+      </div>
     </div>
   </Dialog>
 </template>
