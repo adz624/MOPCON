0ee6930b3c20bcd8916a63ba17c21a09f4c20a89
Yuuna <k9532121@gmail.com>
2021-07-09 20:24:36 +0800

    Add Footer component

---
+++ 2021-dev/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <div class="logo-mopcon-s" />
+    <p>堅持濁水溪以南，最大行動科技年會</p>
+  </footer>
+</template>
+<style lang="scss" scoped>
+footer {
+  @include flex(center, column, center);
+  background-color: $colorPrimary;
+  width: 100%;
+  height: 140px;
+  p {
+    color: $colorWhite;
+  }
+}
+</style>
--- 2021-dev/layouts/default.vue
+++ 2021-dev/layouts/default.vue
@@ -2,14 +2,17 @@
   <div>
     <Navbar :is-mobile="isMobile" />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
+import Footer from '~/components/Footer.vue'
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
   computed: {
     isMobile () {
