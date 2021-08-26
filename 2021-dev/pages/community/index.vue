5e650f950fdacdc8c92debb292db23c37c80e356
Eric Fang <ree0916tw@gmail.com>
2021-08-27 03:53:42 +0800

    fix path

--- 2021-dev/pages/community/index.vue
+++ 2021-dev/pages/community/index.vue
@@ -204,7 +204,7 @@ export default {
           id: 1,
           name: 'PyCon Taiwan',
           name_ch: 'Python 年會',
-          logo_file_name: 'pyConTaiwan',
+          logo_file_name: 'PyConTaiwan',
           intro: 'PyCon ，亦即 Python 年會，是全球 Python 社群年度盛會。PyCon 由各地同好自發籌辦，而現在台灣也有自己的 Python 年會了。這個活動旨在聚集台灣各領域應用 Python 的人，交流彼此的經驗心得，分享最新的技術發展，並親身體驗社群獨有的熱情。年會除了有從 Python 的語言特性到各方面應用案例實例的精彩演講之外，我們也很重視與會者之間的交流與促成開發者與廠商合作可能，提供創業者交流討論的園地，達到社群技能和發展的目的。',
           fb: 'https://www.facebook.com/pycontw/',
           website: 'https://tw.pycon.org/2021/zh-hant'
@@ -275,14 +275,14 @@ export default {
     },
     getVolunteerList () {
       const vm = this
-      vm.$axios.$get('volunteer.json')
+      vm.$axios.$get(process.env.BASE_URL + '/2021/volunteer.json')
         .then((data) => {
           vm.volunteerList = data
         })
     },
     getVolunteerData (name) {
       const vm = this
-      vm.$axios.$get('volunteer-' + name + '.json')
+      vm.$axios.$get(process.env.BASE_URL + '/2021/volunteer-' + name + '.json')
         .then((data) => {
           vm.volunteerData = data
         })
