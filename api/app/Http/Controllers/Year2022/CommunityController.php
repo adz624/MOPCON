5b8ba52f7d1a690cd8afbd9ebac1f9425194885b
loxyztw <loxyz.tw@gmail.com>
2022-06-14 23:25:56 +0800

    Added news id and key in env.example;added folder Year2022 in Controllers; updated functions in BaseNewsController; updated routes.

---
+++ api/app/Http/Controllers/Year2022/CommunityController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Year2022;
+
+use App\Http\Controllers\BaseCommunityController;
+
+class CommunityController extends BaseCommunityController
+{
+    protected $year = 2022;
+}
---
+++ api/app/Http/Controllers/Year2022/SessionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Year2022;
+
+use App\Http\Controllers\BaseSessionController;
+
+class SessionController extends BaseSessionController
+{
+    protected $year = 2022;
+}
