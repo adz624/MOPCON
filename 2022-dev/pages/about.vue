e7c72655d8eb84df7b863f4943a1cd9c8099b508
Chi-Chi-Chi-Chita <chita0901@gmail.com>
2022-07-08 23:29:35 +0800

    create 2022-dev

---
+++ 2022-dev/pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="title">
+    123
+    <p class="content">
+      456
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style lang="scss" scoped>
+.title{
+  color:red;
+  .content{
+    color:$colorPink;
+  }
+}
+
+</style>
---
+++ 2022-dev/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    首頁
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
