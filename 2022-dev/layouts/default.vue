4429c6fc7f28f8d04ceced3c1c8d6bd518fec544
Yuuna <k9532121@gmail.com>
2022-07-23 23:14:04 +0800

    Add 2022 Navbar and Footer component

---
+++ 2022-dev/layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Navbar :is-mobile="isMobile" />
+    <nuxt class="page" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+import Footer from '~/components/Footer.vue'
+export default {
+  name: 'DefaultLayout',
+  components: {
+    Navbar,
+    Footer
+  },
+  data () {
+    return {
+      innerWidth: null
+    }
+  },
+  computed: {
+    isMobile () {
+      return this.innerWidth <= 1024
+    }
+  },
+  mounted () {
+    this.innerWidth = window.innerWidth
+    window.addEventListener('resize', this.resize)
+  },
+  methods: {
+    resize () {
+      this.innerWidth = window.innerWidth
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  padding-top: 112px;
+}
+</style>
