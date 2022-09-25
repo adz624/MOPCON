c93665efeffda8740d8420f766ee483d4e225ea7
Yu <kuan.yu.contact@gmail.com>
2022-09-26 01:25:13 +0800

    feat: :sparkles: init mosume page in 2022

---
+++ 2022-dev/pages/mosume/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="Mosume">
+    <SectionBanner />
+    <SectionAbout />
+    <SectionYoutube />
+    <SectionDesignThought />
+    <SectionHistory />
+  </div>
+</template>
+
+<script>
+import SectionBanner from './SectionBanner'
+import SectionAbout from './SectionAbout'
+import SectionYoutube from './SectionYoutube'
+import SectionDesignThought from './SectionDesignThought'
+import SectionHistory from './SectionHistory'
+
+export default {
+  name: 'Mosume',
+  components: {
+    SectionBanner,
+    SectionAbout,
+    SectionYoutube,
+    SectionDesignThought,
+    SectionHistory
+  },
+  layout: 'mosume'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
