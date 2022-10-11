a26974c22bba1865657db4d4fd9d335b8211f8bb
Yuuna <k9532121@gmail.com>
2022-10-12 00:39:48 +0800

    feat: adjust 2022 edashboard style

--- 2022-dev/components/EboardSponsor/index.vue
+++ 2022-dev/components/EboardSponsor/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div v-if="sponsorData.sponsor_ad_img" class="eboardSponsor">
-    <img src="./images/circle.png" width="635" alt="" class="bg-circle-1">
-    <img src="./images/circle.png" width="872" alt="" class="bg-circle-2">
     <div class="sponsor__info" :class="{'hasNewsheight': hasNews}">
       <div class="area logo-bg" style="overflow: hidden;">
         <img :src="sponsorData.sponsor_ad_img" alt="" class="sponsor__img">
