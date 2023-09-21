e9b1f30954b1140e4c8e771b4417823b084831af
Mikimoto <mikimoto.c@gmail.com>
2023-09-22 00:09:07 +0800

    [test] auto build

--- adm/_github_hook.php
+++ adm/_github_hook.php
@@ -52,7 +52,8 @@ exec("cd {$doc_root} && /usr/local/nvm/versions/node/v8.11.0/bin/npm install");
 //exec("cd {$doc_root}2019-dev && /usr/local/nvm/versions/node/v8.11.0/bin/yarn run generate");
 
 // Build 2023 web site
-exec(cd {$doc_root}2023-dev && make);
+exec("cd {$doc_root}2023-dev && make", $output, $return_var);
+echo "Result: {$return_var}, {$output}"
 
 // 如果有 memcache，把最新的 deploy 狀況寫入 memcache
 $memcache_ok = function_exists("memcache_connect");
