7bef28afdd2f73c17058562a1d143016d2e2019d
YZRay1119 <yao19991119@mopcon.org>
2023-10-18 21:20:41 +0800

    新增贊助商圖片

--- 2023-dev/src/page/Sponsor/SponsorArea.js
+++ 2023-dev/src/page/Sponsor/SponsorArea.js
@@ -1,14 +1,14 @@
 import { Fragment, useState } from "react";
 import SponsorSection from "./SponsorSection";
 // Icon
-import { ReactComponent as UniverseIcon } from "../../components/asset/sponsorIcon/universe.svg";
+// import { ReactComponent as UniverseIcon } from "../../components/asset/sponsorIcon/universe.svg";
 import { ReactComponent as GalaxyIcon } from "../../components/asset/sponsorIcon/galaxy.svg";
 import { ReactComponent as PlanetIcon } from "../../components/asset/sponsorIcon/planet.svg";
 import { ReactComponent as CometIcon } from "../../components/asset/sponsorIcon/comet.svg";
 import { ReactComponent as EducationIcon } from "../../components/asset/sponsorIcon/cil_education.svg";
 import { ReactComponent as ThanksIcon } from "../../components/asset/sponsorIcon/ph_hand-heart-thin.svg";
 //Sponsor
-import UniverseSponsor from "../../data/Sponsor/UniverseSponsor.json";
+// import UniverseSponsor from "../../data/Sponsor/UniverseSponsor.json";
 import GalaxySponsor from "../../data/Sponsor/GalaxySponosr.json";
 import PlanetSponsor from "../../data/Sponsor/PlanetSponsor.json";
 import CometSponsor from "../../data/Sponsor/CometSponsor.json";
@@ -39,12 +39,6 @@ const SponsorArea = () => {
       )}
       <h2>我們的夥伴們</h2>
       <SponsorSection
-        Icon={<UniverseIcon />}
-        Content={UniverseSponsor}
-        Level="宇宙級"
-        onCardClick={openModalHandle}
-      />
-      <SponsorSection
         Icon={<GalaxyIcon />}
         Content={GalaxySponsor}
         Level="銀河級"
@@ -68,12 +62,12 @@ const SponsorArea = () => {
         Level="教育贊助"
         onCardClick={openModalHandle}
       />
-      <SponsorSection
+      {/* <SponsorSection
         Icon={<ThanksIcon />}
         Content={SpecialThanksSponsor}
         Level="特別感謝"
         onCardClick={openModalHandle}
-      />
+      /> */}
     </Fragment>
   );
 };
